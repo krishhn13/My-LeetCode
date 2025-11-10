d85b1d4e0179dd39848be2c778a78bb7b14f3842
Krishan <chiranjiv.shah783@gmail.com>
2025-11-10 09:44:40 +0530

    Release Naive Solution for Fibonacci Number

--- LeetCode 509 - Fibonacci Number/Naive.java
+++ LeetCode 509 - Fibonacci Number/Naive.java
@@ -1,9 +1,19 @@
 class Solution {
         public int fib(int n) {
-                return 0;
+                int a = 0 ,b = 1;
+                int c = 0;
+                for(int i = 2; i < n; i++) {
+                        c= a+b;
+                        a = b;
+                        b = c;
+                }
+                return c;
         }
 }
 
 public class Naive {
-
+        public static void main(String[] args) {
+                Solution sol = new Solution();
+                System.out.println(sol.fib(7));
+        }
 }
