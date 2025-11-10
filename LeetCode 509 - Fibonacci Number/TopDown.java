f162ddfaa97b047f29d2e8ebf34a3d2ffeb0ac1a
Krishan <chiranjiv.shah783@gmail.com>
2025-11-10 09:54:43 +0530

    Released Top-Down Approach of Fibonacci Number

--- LeetCode 509 - Fibonacci Number/TopDown.java
+++ LeetCode 509 - Fibonacci Number/TopDown.java
@@ -1,3 +1,25 @@
+import java.util.Arrays;
+
+class Solution{
+        public int fib(int n){
+                int[] dp = new int[n+1];
+                Arrays.fill(dp,-1);
+                return fibo(n,dp);
+        }
+        int fibo(int n, int[] dp) {
+                if(n <= 1) {
+                        return n;
+                }
+                if(dp[n]!=-1) {
+                        return dp[n];
+                } 
+                dp[n] = fibo(n-1,dp) + fibo(n-2,dp);
+                return dp[n];
+        }
+}
 public class TopDown {
-        
+        public static void main(String[] args) {
+                Solution sol = new Solution();
+                System.out.println(sol.fib(5));
+        }
 }
