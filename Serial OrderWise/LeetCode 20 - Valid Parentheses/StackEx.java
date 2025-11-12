caca37ffe26431261f3611ba4dc051680195379d
Krishan <chiranjiv.shah783@gmail.com>
2025-11-12 21:07:11 +0530

    Released Solution for Valid Parentheses

---
+++ Serial OrderWise/LeetCode 20 - Valid Parentheses/StackEx.java
@@ -0,0 +1,28 @@
+import java.util.*;
+class Solution {
+    public boolean isValid(String s) {
+        if (s.length() <= 1) return false;
+        Stack<Character> stack = new Stack<>();
+        for (char c : s.toCharArray()) {
+            if (c == '(' || c == '{' || c == '[') {
+                stack.push(c);
+            } else {
+                if (stack.isEmpty()) return false;
+                char top = stack.pop();
+                if ((c == ')' && top != '(') ||
+                    (c == '}' && top != '{') ||
+                    (c == ']' && top != '[')) {
+                    return false;
+                }
+            }
+        }
+        return stack.isEmpty();
+    }
+}
+
+public class StackEx {
+        public static void main(String[] args) {
+            Solution sol = new Solution();
+            System.out.println(sol.isValid("[]{}{{(}}"));
+        }
+}
