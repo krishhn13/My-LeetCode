03b79823e6b7e530ca317a5872bc61fdf94722eb
Krishan <chiranjiv.shah783@gmail.com>
2025-11-13 14:24:36 +0530

    Update

---
+++ Topic Wise/Greedy/LeetCode3228.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int maxOperations(String s) {
+        int ans = 0, count1 = 0;
+        int n = s.length();
+        for (int i = 0; i < n; i++) {
+            if (s.charAt(i) == '1')
+                count1++;
+            else if (i > 0 && s.charAt(i - 1) == '1' && s.charAt(i) == '0') {
+                ans += count1;
+            }
+        }
+        return ans;
+    }
+}
+
+public class LeetCode3228 {
+    public static void main(String[] args) {
+        Solution sol = new Solution();
+        System.out.println(sol.maxOperations("0011010101100"));
+    }
+}
