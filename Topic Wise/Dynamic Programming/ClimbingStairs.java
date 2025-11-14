2c0478c8fea7fb918d91ec795210bdc464d38caa
Krishan <chiranjiv.shah783@gmail.com>
2025-11-14 20:43:17 +0530

    Released Sol for LeetCode 70

---
+++ Topic Wise/Dynamic Programming/ClimbingStairs.java
@@ -0,0 +1,30 @@
+import java.util.*;
+
+class Solution {
+        public int climbStairs(int n) {
+                int[] dp = new int[n + 2];
+                Arrays.fill(dp, -1);
+                count(0, n, dp);
+                System.out.println(Arrays.toString(dp));
+                return 0;
+        }
+
+        int count(int i, int n, int[] dp) {
+                if (i == n)
+                        return 1;
+                if (i > n)
+                        return 0;
+                if (dp[i] != -1) {
+                        return dp[i];
+                }
+                dp[i] = count(i + 1, n, dp) + count(i + 2, n, dp);
+                return dp[0];
+        }
+}
+
+public class ClimbingStairs {
+        public static void main(String[] args) {
+                Solution sol = new Solution();
+                System.out.println(sol.climbStairs(44));
+        }
+}
