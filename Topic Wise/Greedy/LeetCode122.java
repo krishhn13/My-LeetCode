bf3731a39e012018d501d5a4fe3b94222dd0a585
Krishan <chiranjiv.shah783@gmail.com>
2025-11-16 19:12:47 +0530

    Released Solution for LeetCode 122

--- Topic Wise/Greedy/LeetCode122.java
+++ Topic Wise/Greedy/LeetCode122.java
@@ -1,25 +1,25 @@
 class Solution {
-    public int maxProfit(int[] prices) {
-        int profit = -prices[0];
-        int pre = prices[0];
-        for(int i = 1;i < prices.length;i++) {
-            if(prices[i]>pre) {
-                profit+=prices[i];
-            }
-            else {
-                profit+= pre;
-            }
-            pre = prices[i];
-            profit -= pre;
+        public int maxProfit(int[] prices) {
+                int profit = -prices[0];
+                int pre = prices[0];
+                for (int i = 1; i < prices.length; i++) {
+                        if (prices[i] > pre) {
+                                profit += prices[i];
+                        } else {
+                                profit += pre;
+                        }
+                        pre = prices[i];
+                        profit -= pre;
+                }
+                profit += pre;
+                return profit;
         }
-        profit+=pre;
-        return profit;   
-    }
 }
+
 public class LeetCode122 {
         public static void main(String[] args) {
                 Solution sol = new Solution();
-                int[] arr = {1,3,4,5,6,7};
+                int[] arr = { 1, 3, 4, 5, 6, 7 };
                 System.out.println(sol.maxProfit(arr));
         }
 }
