e5c8d6994c9b28183f3b3209f5164ab36d705a30
Krishan <chiranjiv.shah783@gmail.com>
2025-11-16 19:08:28 +0530

    Released Solution for LeetCode 1827

---
+++ Topic Wise/Greedy/LeetCode1827.java
@@ -0,0 +1,21 @@
+class Solution {
+        public int minOperations(int[] nums) {
+                if (nums.length == 1)
+                        return 0;
+                int count = 0;
+                for (int i = 1; i < nums.length; i++) {
+                        if (nums[i] <= nums[i - 1]) {
+                                count += (nums[i - 1] - nums[i] + 1);
+                                nums[i] += (nums[i - 1] - nums[i] + 1);
+                        }
+                }
+                return count;
+        }
+
+public class LeetCode1827 {
+         public static void main(String[] args) {
+                Solution sol = new Solution();
+                int[] arr = {1,24,45,23,11,90};
+                System.out.println(sol.minOperations(arr));
+        }
+}
