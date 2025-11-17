0a1878588e06feb6137aa2cdd83f0cbed14ebfd5
Krishan <chiranjiv.shah783@gmail.com>
2025-11-17 20:17:21 +0530

    Released Solution for SAME TREE

--- Topic Wise/Trees/LeetCode100.java
+++ Topic Wise/Trees/LeetCode100.java
@@ -19,17 +19,13 @@ class TreeNode {
 
 class Solution {
         public boolean isSameTree(TreeNode p, TreeNode q) {
-                if (p == null && q == null)
-                        return true;
-                if (p == null && q != null)
-                        return false;
-                if (p != null && q == null)
-                        return false;
+                if (p == null && q == null) return true;
+                if (p == null && q != null) return false;
+                if (p != null && q == null) return false;
                 boolean left = isSameTree(p.left, q.left);
                 boolean right = isSameTree(p.right, q.right);
                 boolean val = p.val == q.val;
                 return left && right && val;
-
         }
 }
 
