356b754b4634da181089872da1706a0018ac27e7
Krishan <chiranjiv.shah783@gmail.com>
2025-11-17 20:15:32 +0530

    Released Solution for SAME TREE

--- Topic Wise/Trees/LeetCode100.java
+++ Topic Wise/Trees/LeetCode100.java
@@ -29,11 +29,26 @@ class Solution {
                 boolean right = isSameTree(p.right, q.right);
                 boolean val = p.val == q.val;
                 return left && right && val;
+
         }
 }
 
 public class LeetCode100 {
         public static void main(String[] args) {
+                Solution sol = new Solution();
 
+                // Test 1: identical trees [1,2,3]
+                TreeNode p1 = new TreeNode(1, new TreeNode(2), new TreeNode(3));
+                TreeNode q1 = new TreeNode(1, new TreeNode(2), new TreeNode(3));
+                System.out.println("Test1 (expected true): " + sol.isSameTree(p1, q1));
+
+                // Test 2: different structure [1,2] vs [1,null,2]
+                TreeNode p2 = new TreeNode(1, new TreeNode(2), null);
+                TreeNode q2 = new TreeNode(1, null, new TreeNode(2));
+                System.out.println("Test2 (expected false): " + sol.isSameTree(p2, q2));
+
+                // Test 3: both null
+                System.out.println("Test3 (expected true): " + sol.isSameTree(null, null));
         }
+
 }
