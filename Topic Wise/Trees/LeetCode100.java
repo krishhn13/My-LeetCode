4cb8b534a830bddc4617c66f3f11441432767de2
Krishan <chiranjiv.shah783@gmail.com>
2025-11-17 20:17:30 +0530

    Released Solution for SAME TREE

--- Topic Wise/Trees/LeetCode100.java
+++ Topic Wise/Trees/LeetCode100.java
@@ -2,14 +2,11 @@ class TreeNode {
         int val;
         TreeNode left;
         TreeNode right;
-
         TreeNode() {
         }
-
         TreeNode(int val) {
                 this.val = val;
         }
-
         TreeNode(int val, TreeNode left, TreeNode right) {
                 this.val = val;
                 this.left = left;
