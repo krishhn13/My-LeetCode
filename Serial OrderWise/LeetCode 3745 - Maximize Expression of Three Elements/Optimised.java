77b72c15e5b2c042813ea6378ecedc3db68f028d
Krishan <chiranjiv.shah783@gmail.com>
2025-11-18 20:31:56 +0530

    Released Solution for Leetcode 3745

---
+++ Serial OrderWise/LeetCode 3745 - Maximize Expression of Three Elements/Optimised.java
@@ -0,0 +1,31 @@
+class Solution {
+    public int maximizeExpressionOfThree(int[] nums) {
+        int mini = Integer.MAX_VALUE;
+        int maxi = Integer.MIN_VALUE;
+        int secMaxi = Integer.MIN_VALUE;
+        int check = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] > maxi) {
+                maxi = nums[i];
+                check = i;
+            }
+            if (nums[i] < mini) {
+                mini = nums[i];
+            }
+        }
+        for (int i = 0; i < nums.length; i++) {
+            if (i != check && nums[i] > secMaxi) {
+                secMaxi = nums[i];
+            }
+        }
+        return maxi + secMaxi - mini;
+
+    }
+}
+
+public class Optimised {
+    public static void main(String[] args) {
+        Solution sol = new Solution();
+        sol.maximizeExpressionOfThree(new int[] { 4, 12, 3, 1, 0 });
+    }
+}
