e991bcb1ea5df52ffe24e0f9eac0f221f7795989
Krishan <chiranjiv.shah783@gmail.com>
2025-11-18 20:26:48 +0530

    Released Solution for LeetCode 374

--- Serial OrderWise/LeetCode 374 - Guess Number Higher or Lower/Answer.java
+++ Serial OrderWise/LeetCode 374 - Guess Number Higher or Lower/Answer.java
@@ -1,41 +1,50 @@
-/** 
+/**
  * Forward declaration of guess API.
- * @param  num   your guess
- * @return 	     -1 if num is higher than the picked number
- *			      1 if num is lower than the picked number
- *               otherwise return 0
- * int guess(int num);
+ * 
+ * @param num your guess
+ * @return -1 if num is higher than the picked number
+ *         1 if num is lower than the picked number
+ *         otherwise return 0
+ *         int guess(int num);
  */
 class GuessGame {
         int pick;
-        GuessGame(){
+
+        GuessGame() {
 
         }
+
         GuessGame(int pick) {
                 this.pick = pick;
         }
+
         int guess(int num) {
-                if(num==pick) return 0;
-                else if(num<0) return -1;
-                else return 1;
+                if (num == pick)
+                        return 0;
+                else if (num < 0)
+                        return -1;
+                else
+                        return 1;
         }
 }
- class Solution extends GuessGame {
-    public int guessNumber(int n) {
-        int s = 1, l = n;
-        while(s<=l) {
-            int mid = s+(l-s)/2;
-            if(guess(mid) == 0) {
-                return mid;
-            } else if(guess(mid) == -1) {
-                l = mid-1;
-            } else {
-                s = mid+1;
-            }
+
+class Solution extends GuessGame {
+        public int guessNumber(int n) {
+                int s = 1, l = n;
+                while (s <= l) {
+                        int mid = s + (l - s) / 2;
+                        if (guess(mid) == 0) {
+                                return mid;
+                        } else if (guess(mid) == -1) {
+                                l = mid - 1;
+                        } else {
+                                s = mid + 1;
+                        }
+                }
+                return -1;
         }
-        return -1;
-    }
 }
+
 public class Answer {
         public static void main(String[] args) {
                 GuessGame guessGame = new GuessGame(6);
