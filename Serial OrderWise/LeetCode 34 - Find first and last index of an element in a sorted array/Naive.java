cc035045a1ab70b0332ab3e5b535432aabf7cd26
Krishan <chiranjiv.shah783@gmail.com>
2025-11-19 20:32:11 +0530

    Worked upon the formattin of the code

--- Serial OrderWise/LeetCode 34 - Find first and last index of an element in a sorted array/Naive.java
+++ Serial OrderWise/LeetCode 34 - Find first and last index of an element in a sorted array/Naive.java
@@ -1,24 +1,25 @@
 class Solution {
-    public int[] searchRange(int[] nums, int target) {
-        int f = -1, l = -1;
-        for (int i = 0; i < nums.length; i++) {
-            if (nums[i] == target) {
-                f = i;
-                break;
-            }
+        public int[] searchRange(int[] nums, int target) {
+                int f = -1, l = -1;
+                for (int i = 0; i < nums.length; i++) {
+                        if (nums[i] == target) {
+                                f = i;
+                                break;
+                        }
+                }
+                for (int i = nums.length - 1; i >= 0; i--) {
+                        if (target == nums[i]) {
+                                l = i;
+                                break;
+                        }
+                }
+                return new int[] { f, l };
         }
-        for (int i = nums.length - 1; i >= 0; i--) {
-            if (target == nums[i]) {
-                l = i;
-                break;
-            }
-        }
-        return new int[] { f, l };
-    }
 }
+
 public class Naive {
         public static void main(String[] args) {
                 Solution sol = new Solution();
-                System.out.println(sol.searchRange( new int[]{5,7,7,8,8,10}, 8));
+                System.out.println(sol.searchRange(new int[] { 5, 7, 7, 8, 8, 10 }, 8));
         }
 }
