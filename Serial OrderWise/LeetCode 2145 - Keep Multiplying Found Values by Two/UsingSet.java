30ac4816567a724a04c4869f7085418426d05ae9
Krishan <chiranjiv.shah783@gmail.com>
2025-11-19 09:23:30 +0530

    Released Daily Question

---
+++ Serial OrderWise/LeetCode 2145 - Keep Multiplying Found Values by Two/UsingSet.java
@@ -0,0 +1,19 @@
+import java.util.*;
+class Solution {
+    public int findFinalValue(int[] nums, int original) {
+        Set<Integer> set = new HashSet<>();
+        for(int i : nums) {
+            set.add(i);
+        }
+        while(set.contains(original)) {
+            original*=2;
+        }
+        return original;
+    }
+}
+public class UsingSet {
+        public static void main(String[] args) {
+                Solution sol = new Solution();
+                sol.findFinalValue(new int[]{161,28,640,264,81,561,320,2,61,244,183,108,773,61,976,122,988,2,370,392,488,375,349,432,713,563}, 61);
+        }
+}
