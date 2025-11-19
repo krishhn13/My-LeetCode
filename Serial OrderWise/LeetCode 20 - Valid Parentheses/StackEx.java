19bef8e059a7cad40177c494c7c38c019d970011
Krishan <chiranjiv.shah783@gmail.com>
2025-11-19 20:37:58 +0530

    Worked on formatting

--- Serial OrderWise/LeetCode 20 - Valid Parentheses/StackEx.java
+++ Serial OrderWise/LeetCode 20 - Valid Parentheses/StackEx.java
@@ -1,17 +1,20 @@
 import java.util.*;
+
 class Solution {
     public boolean isValid(String s) {
-        if (s.length() <= 1) return false;
+        if (s.length() <= 1)
+            return false;
         Stack<Character> stack = new Stack<>();
         for (char c : s.toCharArray()) {
             if (c == '(' || c == '{' || c == '[') {
                 stack.push(c);
             } else {
-                if (stack.isEmpty()) return false;
+                if (stack.isEmpty())
+                    return false;
                 char top = stack.pop();
                 if ((c == ')' && top != '(') ||
-                    (c == '}' && top != '{') ||
-                    (c == ']' && top != '[')) {
+                        (c == '}' && top != '{') ||
+                        (c == ']' && top != '[')) {
                     return false;
                 }
             }
@@ -21,8 +24,8 @@ class Solution {
 }
 
 public class StackEx {
-        public static void main(String[] args) {
-            Solution sol = new Solution();
-            System.out.println(sol.isValid("[]{}{{(}}"));
-        }
+    public static void main(String[] args) {
+        Solution sol = new Solution();
+        System.out.println(sol.isValid("[]{}{{(}}"));
+    }
 }
