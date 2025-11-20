311b469b0f48ca760e82b8c6d4aa5b3460b5d3c6
Krishan <chiranjiv.shah783@gmail.com>
2025-11-20 21:53:54 +0530

    Released Solution for LeetCode 1507

---
+++ Serial OrderWise/LeetCode 1507 - Reformat Date/Answer.java
@@ -0,0 +1,41 @@
+class Solution {
+    public String reformatDate(String date) {
+        String[] ans = date.split(" ");
+        // System.out.println(Arrays.toString(ans));
+        StringBuilder str = new StringBuilder();
+        str.append(ans[2]);
+        str.append("-");
+        str.append(getMonth(ans[1]));
+        str.append("-");
+        if(ans[0].length()==3) {
+            str.append("0"+ans[0].charAt(0));
+        } else {
+            str.append(ans[0].charAt(0)); 
+            str.append(ans[0].charAt(1));
+        }
+        return str.toString();
+    }
+    String getMonth(String mon){
+        switch(mon){
+            case "Jan" : return "01";
+            case "Feb" : return "02";
+            case "Mar" : return "03";
+            case "Apr" : return "04";
+            case "May" : return "05";
+            case "Jun" : return "06";
+            case "Jul" : return "07";
+            case "Aug" : return "08";
+            case "Sep" : return "09";
+            case "Oct" : return "10";
+            case "Nov" : return "11";
+            case "Dec" : return "12";
+        }
+        return "";
+    }
+}
+public class Answer {
+        public static void main(String[] args) {
+            Solution sol = new Solution();
+            System.out.println(sol.reformatDate("13th Nov 2005"));
+        }
+}
