778a5b14ca52a88effbd0e39464c9548bc51517a
Krishan <chiranjiv.shah783@gmail.com>
2025-11-20 21:37:54 +0530

    Released Solution for 1832 using sets

--- Serial OrderWise/LeetCode 1832 - Check if the sentence is Pangram/UsingSet.java
+++ Serial OrderWise/LeetCode 1832 - Check if the sentence is Pangram/UsingSet.java
@@ -1,14 +1,17 @@
 import java.util.*;
+
 class Solution {
-    public boolean checkIfPangram(String sentence) {
-       Set<Character>set = new HashSet<>();
-       for(char c :sentence.toCharArray()) set.add(c);
-       return set.size()==26;
-    }
+        public boolean checkIfPangram(String sentence) {
+                Set<Character> set = new HashSet<>();
+                for (char c : sentence.toCharArray())
+                        set.add(c);
+                return set.size() == 26;
+        }
 }
+
 public class UsingSet {
         public static void main(String[] args) {
-            Solution sol = new Solution();
-            System.out.println(sol.checkIfPangram("krishn"));
+                Solution sol = new Solution();
+                System.out.println(sol.checkIfPangram("krishn"));
         }
 }
