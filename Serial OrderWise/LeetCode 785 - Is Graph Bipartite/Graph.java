0b5996c707e7ef2201a0f97ab2e7075cfea90277
Krishan <chiranjiv.shah783@gmail.com>
2025-11-21 21:23:37 +0530

    Released Solution for leetcode 785

---
+++ Serial OrderWise/LeetCode 785 - Is Graph Bipartite/Graph.java
@@ -0,0 +1,46 @@
+import java.util.*;
+
+class Solution {
+        public boolean isBipartite(int[][] graph) {
+                int n = graph.length;
+                int[] color = new int[n];
+                Arrays.fill(color, -1);
+
+                for (int i = 0; i < n; i++) {
+                        if (color[i] == -1) {
+                                if (!dfs(i, 0, color, graph))
+                                        return false;
+                        }
+                }
+                return true;
+        }
+
+        boolean dfs(int node, int c, int[] color, int[][] graph) {
+                color[node] = c;
+                for (int it : graph[node]) {
+                        if (color[it] == -1) {
+                                if (!dfs(it, 1 - c, color, graph))
+                                        return false;
+                        } else if (color[it] == c) {
+                                return false;
+                        }
+                }
+                return true;
+        }
+}
+
+
+
+public class Graph {
+         public static void main(String[] args) {
+                Solution sol = new Solution();
+                int[][] graph = { 
+                        { 1, 2, 3 }, 
+                        { 0, 2 },
+                        { 0, 1, 3 },
+                        { 0, 2 } 
+                };
+                System.out.println(sol.isBipartite(graph));
+        
+        }
+}
