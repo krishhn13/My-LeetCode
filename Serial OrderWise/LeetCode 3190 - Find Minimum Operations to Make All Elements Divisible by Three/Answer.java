082c62c1999eb5ec0342d4310333d74db35e17fb
Krishan <chiranjiv.shah783@gmail.com>
2025-11-22 15:17:24 +0530

    Released Solution for LeetCode 3190

--- Serial OrderWise/LeetCode 3190 - Find Minimum Operations to Make All Elements Divisible by Three/Answer.java
+++ Serial OrderWise/LeetCode 3190 - Find Minimum Operations to Make All Elements Divisible by Three/Answer.java
@@ -1,18 +1,18 @@
 class Solution {
-    public int minimumOperations(int[] nums) {
-        int count = 0;
-        for (int i = 0; i < nums.length; i++) {
-            if (nums[i] % 3 != 0) { 
-                nums[i]++;
-                count++;
-            }       
+        public int minimumOperations(int[] nums) {
+                int count = 0;
+                for (int i = 0; i < nums.length; i++) {
+                        if (nums[i] % 3 != 0) {
+                                count++;
+                        }
+                }
+                return count;
         }
-        return count;
-    }
 }
+
 public class Answer {
         public static void main(String[] args) {
                 Solution sol = new Solution();
-                System.out.println(sol.minimumOperations(new int[]{1,2,3,4}));
+                System.out.println(sol.minimumOperations(new int[] { 1, 2, 3, 4 }));
         }
 }
