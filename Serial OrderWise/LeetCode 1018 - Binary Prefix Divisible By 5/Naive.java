a3ffe4f413937805df58389eea6bf2f148c67507
Krishan <chiranjiv.shah783@gmail.com>
2025-11-24 19:41:44 +0530

    Released Solution for LeetCode 1018

---
+++ Serial OrderWise/LeetCode 1018 - Binary Prefix Divisible By 5/Naive.java
@@ -0,0 +1,18 @@
+import java.util.*;
+class Solution {
+    public List<Boolean> prefixesDivBy5(int[] nums) {
+        List<Boolean> list = new ArrayList<>();
+        int x = 0 ;
+        for(int i : nums) {
+            x = (2*x + i)%5;
+            list.add(x==0);
+        }
+        return list;
+    }
+}
+public class Naive {
+        public static void main(String[] args) {
+                Solution sol = new Solution();
+                System.out.println(prefixesDivBy5(new int[]{0,1,1}));
+        }    
+}
