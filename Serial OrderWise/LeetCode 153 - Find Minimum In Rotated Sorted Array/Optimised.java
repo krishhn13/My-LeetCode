0b4c4e59eb6747a05f3da7db9fd322e2d6d58b30
Krishan <chiranjiv.shah783@gmail.com>
2025-12-06 18:40:47 +0530

    Released Optimised Solution for LC 153

---
+++ Serial OrderWise/LeetCode 153 - Find Minimum In Rotated Sorted Array/Optimised.java
@@ -0,0 +1,21 @@
+class Solution{
+        public int findMin(int[] arr) {
+                int left = 0, right = arr.length-1;
+                while (left<right) {
+                        int mid = left + (right-left)/2;
+                        if(arr[mid]>arr[right]) {
+                                left = mid+1;
+                        } else {
+                                right = mid;
+                        }
+                }
+                return arr[left];
+        }
+}
+public class Optimised {
+        public static void main(String[] args) {
+                Solution sol = new Solution();
+                int[] arr = {3,4,5,1,2};
+                System.out.println(sol.findMin(arr));
+        }
+}
