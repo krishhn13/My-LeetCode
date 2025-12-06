9cbdcc9c7411e7d8f981231d68099623391251db
Krishan <chiranjiv.shah783@gmail.com>
2025-12-06 19:05:03 +0530

    Released Optimised Solution for LC 154

--- Serial OrderWise/LeetCode 154 - Find Minimum In Rotated Sorted Array II/Optimised.java
+++ Serial OrderWise/LeetCode 154 - Find Minimum In Rotated Sorted Array II/Optimised.java
@@ -1,27 +1,28 @@
 class Solution {
-    public int findMin(int[] nums) {
-        int left = 0;
-        int right = nums.length - 1;
-        if (nums[left]<nums[right]) {
-            return nums[left];
+        public int findMin(int[] nums) {
+                int left = 0;
+                int right = nums.length - 1;
+                if (nums[left] < nums[right]) {
+                        return nums[left];
+                }
+                while (left <= right) {
+                        int mid = left + (right - left) / 2;
+                        if (nums[mid] > nums[right]) {
+                                left = mid + 1;
+                        } else if (nums[mid] < nums[right]) {
+                                right = mid;
+                        } else {
+                                right--;
+                        }
+                }
+                return nums[left];
         }
-        while (left <= right) {
-            int mid = left + (right - left) / 2;
-            if (nums[mid] > nums[right]) {
-                left = mid + 1;
-            } else if (nums[mid] < nums[right]) {
-                right = mid;
-            } else {
-                right--;
-            }
-        }
-        return nums[left];
-    }
 }
+
 public class Optimised {
         public static void main(String[] args) {
                 Solution sol = new Solution();
-                int[] arr = {2,2,2,0,1};
+                int[] arr = { 2, 2, 2, 0, 1 };
                 System.out.println(sol.findMin(arr));
         }
 }
