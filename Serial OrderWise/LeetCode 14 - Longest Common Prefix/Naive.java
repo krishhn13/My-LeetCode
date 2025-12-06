9de4a8d79165ab5b9683281063cac4b7009876ba
Krishan <chiranjiv.shah783@gmail.com>
2025-12-06 18:30:57 +0530

    Updated LC 14

--- Serial OrderWise/LeetCode 14 - Longest Common Prefix/Naive.java
+++ Serial OrderWise/LeetCode 14 - Longest Common Prefix/Naive.java
@@ -1,9 +1,7 @@
 class Solution {
     public String longestCommonPrefix(String[] strs) {
         if (strs == null || strs.length == 0) return "";
-
         StringBuilder ans = new StringBuilder();
-
         for (int i = 0; i < strs[0].length(); i++) {
             char a = strs[0].charAt(i);
             for (int j = 1; j < strs.length; j++) {
