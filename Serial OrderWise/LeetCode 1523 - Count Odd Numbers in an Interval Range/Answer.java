345cf329a60512f36bbd0739e63cf370dbb492ee
Krishan <chiranjiv.shah783@gmail.com>
2025-12-08 20:10:38 +0530

    Released Solution for LC 1523

--- Serial OrderWise/LeetCode 1523 - Count Odd Numbers in an Interval Range/Answer.java
+++ Serial OrderWise/LeetCode 1523 - Count Odd Numbers in an Interval Range/Answer.java
@@ -1,10 +1,12 @@
 class Solution {
-    public int countOdds(int low, int high) {
-        int nms=high-low+1; 
-        if(low%2!=0&&high%2!=0) return (nms+1)/2;
-        return nms/2;
-    }
+        public int countOdds(int low, int high) {
+                int nms = high - low + 1;
+                if (low % 2 != 0 && high % 2 != 0)
+                        return (nms + 1) / 2;
+                return nms / 2;
+        }
 }
+
 public class Answer {
         public static void main(String[] args) {
                 Solution sol = new Solution();
