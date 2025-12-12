c4e61ba1dbca717604f4ad060bc3384c0c223687
Krishan <chiranjiv.shah783@gmail.com>
2025-12-12 20:47:24 +0530

    Released Solution for LC 263

--- Serial OrderWise/LeetCode 263 - Ugly Number/Answer.java
+++ Serial OrderWise/LeetCode 263 - Ugly Number/Answer.java
@@ -1,14 +1,15 @@
 class Solution {
-    public boolean isUgly(int n) {
-        if (n <= 0) return false;
-        int[] factors = {2, 3, 5};
-        for (int factor : factors) {
-            while (n % factor == 0) {
-                n /= factor;
-            }
+        public boolean isUgly(int n) {
+                if (n <= 0)
+                        return false;
+                int[] factors = { 2, 3, 5 };
+                for (int factor : factors) {
+                        while (n % factor == 0) {
+                                n /= factor;
+                        }
+                }
+                return n == 1;
         }
-        return n == 1;
-    }
 }
 
 public class Answer {
