d89c19b08d7406df2660bbe8378a6f4741d9d083
Krishan <chiranjiv.shah783@gmail.com>
2025-12-12 20:52:26 +0530

    Released Solution for LeetCode 1961 - Check If String Is a Prefix of Array

--- Serial OrderWise/LeetCode 1961 - Check If String Is a Prefix of Array/Answer.java
+++ Serial OrderWise/LeetCode 1961 - Check If String Is a Prefix of Array/Answer.java
@@ -1,24 +1,24 @@
 class Solution {
-    public boolean isPrefixString(String s, String[] words) {
-        StringBuilder st = new StringBuilder();
-        for (String ch : words) {
-            st.append(ch);
-            if (st.toString().equals(s)) {
-                return true;
-            }
-            if (st.length() > s.length()) {
+        public boolean isPrefixString(String s, String[] words) {
+                StringBuilder st = new StringBuilder();
+                for (String ch : words) {
+                        st.append(ch);
+                        if (st.toString().equals(s)) {
+                                return true;
+                        }
+                        if (st.length() > s.length()) {
+                                return false;
+                        }
+                }
                 return false;
-            }
         }
-        return false;
-    }
 }
 
 public class Answer {
         public static void main(String[] args) {
                 Solution sol = new Solution();
                 String s = "iloveleetcode";
-                String[] arr = {"i","love","leetcode","apples"};
+                String[] arr = { "i", "love", "leetcode", "apples" };
                 System.out.println(sol.isPrefixString(s, arr));
         }
 }
