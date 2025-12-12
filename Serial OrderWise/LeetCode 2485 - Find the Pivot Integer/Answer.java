1fb0a116ad4b8951be5f7410b2c8706c6631be5d
Krishan <chiranjiv.shah783@gmail.com>
2025-12-12 20:49:59 +0530

    Released Solution for LC 2485

--- Serial OrderWise/LeetCode 2485 - Find the Pivot Integer/Answer.java
+++ Serial OrderWise/LeetCode 2485 - Find the Pivot Integer/Answer.java
@@ -1,15 +1,15 @@
 class Solution {
-    public int pivotInteger(int n) {
-        int totalSum = n * (n + 1) / 2;
-        int leftSum = 0;
-        for (int x = 1; x <= n; x++) {
-            leftSum += x; 
-            if (leftSum == totalSum - leftSum + x) { 
-                return x; 
-            }
+        public int pivotInteger(int n) {
+                int totalSum = n * (n + 1) / 2;
+                int leftSum = 0;
+                for (int x = 1; x <= n; x++) {
+                        leftSum += x;
+                        if (leftSum == totalSum - leftSum + x) {
+                                return x;
+                        }
+                }
+                return -1;
         }
-        return -1;
-    }
 }
 
 public class Answer {
