159e7d2b5cfe01f6e0ca2ec2204b7665bfa9cb36
Krishan <chiranjiv.shah783@gmail.com>
2025-12-13 21:16:48 +0530

    Released Solution for LC 1748

--- Serial OrderWise/LeetCode 1748 - Sum of Unique Elements/Answer.java
+++ Serial OrderWise/LeetCode 1748 - Sum of Unique Elements/Answer.java
@@ -1,20 +1,23 @@
 import java.util.*;
+
 class Solution {
-    public int sumOfUnique(int[] nums) {
-        HashMap<Integer,Integer> map = new HashMap<>();
-        for(int i : nums){
-            map.put(i,map.getOrDefault(i,0)+1);
+        public int sumOfUnique(int[] nums) {
+                HashMap<Integer, Integer> map = new HashMap<>();
+                for (int i : nums) {
+                        map.put(i, map.getOrDefault(i, 0) + 1);
+                }
+                int sum = 0;
+                for (int i : map.keySet()) {
+                        if (map.get(i) == 1)
+                                sum += i;
+                }
+                return sum;
         }
-        int sum = 0;
-        for(int i : map.keySet()){
-            if(map.get(i)==1) sum+=i;
-        }
-        return sum;
-    }
 }
+
 public class Answer {
         public static void main(String[] args) {
                 Solution sol = new Solution();
-                System.out.println(sol.sumOfUnique(new int[]{1,1,1,1,1}));
+                System.out.println(sol.sumOfUnique(new int[] { 1, 1, 1, 1, 1 }));
         }
 }
