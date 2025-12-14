eb18afc75f639071ba4cc744facd9f4c2abe6252
Krishan <chiranjiv.shah783@gmail.com>
2025-12-14 10:58:59 +0530

    Released Solution for LC 113

--- Serial OrderWise/LeetCode 113 - Path Sum II/Solution.java
+++ Serial OrderWise/LeetCode 113 - Path Sum II/Solution.java
@@ -1,47 +1,49 @@
 import java.util.*;
+
 class TreeNode {
-    int val;
-    TreeNode left;
-    TreeNode right;
+        int val;
+        TreeNode left;
+        TreeNode right;
 
-    TreeNode() {
-    }
+        TreeNode() {
+        }
 
-    TreeNode(int val) {
-        this.val = val;
-    }
+        TreeNode(int val) {
+                this.val = val;
+        }
 
-    TreeNode(int val, TreeNode left, TreeNode right) {
-        this.val = val;
-        this.left = left;
-        this.right = right;
-    }
+        TreeNode(int val, TreeNode left, TreeNode right) {
+                this.val = val;
+                this.left = left;
+                this.right = right;
+        }
 }
 
 public class Solution {
-    public List<List<Integer>> pathSum(TreeNode root, int targetSum) {
-      List<Integer> pathList = new ArrayList<>();
-    	List<List<Integer>> variousPathList = new ArrayList<>();
+        public List<List<Integer>> pathSum(TreeNode root, int targetSum) {
+                List<Integer> pathList = new ArrayList<>();
+                List<List<Integer>> variousPathList = new ArrayList<>();
 
-		variousPathSum(root, targetSum, pathList, variousPathList);
-		return variousPathList;
-    }
+                variousPathSum(root, targetSum, pathList, variousPathList);
+                return variousPathList;
+        }
 
-	public void variousPathSum(TreeNode root, int targetSum, List<Integer> pathList, List<List<Integer>> variousPathList){
-		
-		if(root == null) return;	
+        public void variousPathSum(TreeNode root, int targetSum, List<Integer> pathList,
+                        List<List<Integer>> variousPathList) {
 
-		pathList.add(root.val);
+                if (root == null)
+                        return;
 
-		if(root.left == null && root.right == null && targetSum-root.val == 0) {
-			variousPathList.add(new ArrayList<>(pathList));
-		} 
-		else {
-			variousPathSum(root.left, targetSum-root.val, pathList, variousPathList);
-			variousPathSum(root.right, targetSum-root.val, pathList, variousPathList);
-		}
+                pathList.add(root.val);
 
-		pathList.remove(pathList.size()-1);
-		
-	}
+                if (root.left == null && root.right == null && targetSum - root.val == 0) {
+                        variousPathList.add(new ArrayList<>(pathList));
+                } else {
+                        variousPathSum(root.left, targetSum - root.val, pathList, variousPathList);
+                        variousPathSum(root.right, targetSum - root.val, pathList, variousPathList);
+                }
+
+                pathList.remove(pathList.size() - 1);
+
+        }
 }
