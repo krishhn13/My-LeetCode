aa8591b757f3b26c16a4df93bde215897f714055
Krishan <chiranjiv.shah783@gmail.com>
2025-12-15 14:40:53 +0530

    Released Solution for LeetCode 209

--- Serial OrderWise/LeetCode 209 - Minimum Size Subarray sum/SlidingWindow.java
+++ Serial OrderWise/LeetCode 209 - Minimum Size Subarray sum/SlidingWindow.java
@@ -1,14 +1,14 @@
-class Solution{
+class Solution {
         public int minSubArrayLen(int target, int[] nums) {
-                int winSt = 0,  sum = 0, minVal = Integer.MAX_VALUE;
-                for(int winEnd = 0; winEnd < nums.length;winEnd++) {
+                int winSt = 0, sum = 0, minVal = Integer.MAX_VALUE;
+                for (int winEnd = 0; winEnd < nums.length; winEnd++) {
                         sum += nums[winEnd];
-                        if(sum>=target) {
-                                int currSize = winEnd-winSt+1;
-                                minVal = Math.min(currSize,minVal);
-                                sum-=nums[winSt];
+                        if (sum >= target) {
+                                int currSize = winEnd - winSt + 1;
+                                minVal = Math.min(currSize, minVal);
+                                sum -= nums[winSt];
                                 winSt++;
-                        }  
+                        }
                 }
                 return minVal == Integer.MAX_VALUE ? 0 : minVal;
         }
@@ -17,6 +17,6 @@ class Solution{
 public class SlidingWindow {
         public static void main(String[] args) {
                 Solution sol = new Solution();
-                System.out.println(sol.minSubArrayLen(7, new int[]{2,3,1,3,4,3}));
+                System.out.println(sol.minSubArrayLen(7, new int[] { 2, 3, 1, 3, 4, 3 }));
         }
 }
