947ba0952c0a45d5b5003a4d9fce93c24ad78d15
Krishan <chiranjiv.shah783@gmail.com>
2025-12-15 14:56:43 +0530

    Released Solution for LC 242

--- Serial OrderWise/LeetCode 242 - Valid Anagram/Naive.java
+++ Serial OrderWise/LeetCode 242 - Valid Anagram/Naive.java
@@ -1,13 +1,15 @@
 import java.util.*;
+
 class Solution {
-    public boolean isAnagram(String s, String t) {
-        char[]ss = s.toCharArray();
-        char[]tt = t.toCharArray();
-        Arrays.sort(ss);
-        Arrays.sort(tt);
-        return (new String(ss).equals(new String(tt)));
-    }
+        public boolean isAnagram(String s, String t) {
+                char[] ss = s.toCharArray();
+                char[] tt = t.toCharArray();
+                Arrays.sort(ss);
+                Arrays.sort(tt);
+                return (new String(ss).equals(new String(tt)));
+        }
 }
+
 public class Naive {
         public static void main(String[] args) {
                 Solution sol = new Solution();
