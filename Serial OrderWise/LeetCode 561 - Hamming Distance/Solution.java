88ba00cd1683369ba4d9c56493a944090503756f
Krishan <chiranjiv.shah783@gmail.com>
2025-12-16 15:49:14 +0530

    Released Naive Solution for LeetCode 561 - Hamming Distance

--- Serial OrderWise/LeetCode 561 - Hamming Distance/Solution.java
+++ Serial OrderWise/LeetCode 561 - Hamming Distance/Solution.java
@@ -1,34 +1,36 @@
 public class Solution {
-    public int hammingDistance(int x, int y) {
-        String a = Integer.toBinaryString(x);
-        String b = Integer.toBinaryString(y);
-        int bada = a.length()-b.length();
-        String c = "";
-        if(bada < 0) {
-            bada*=-1;
-            for(int i = 0;i < bada;i++) {
-                c += "0";
-            }
-            c+=a;
-            a=""; 
-        } else {
-            for(int i = 0;i < bada;i++) {
-                c += "0";
-            }
-            c+=b;
-            b="";
-        } 
-        if(b.equals("")) {
-            return diff(c,a);
-        } else {
-            return diff(c,b);
+        public int hammingDistance(int x, int y) {
+                String a = Integer.toBinaryString(x);
+                String b = Integer.toBinaryString(y);
+                int bada = a.length() - b.length();
+                String c = "";
+                if (bada < 0) {
+                        bada *= -1;
+                        for (int i = 0; i < bada; i++) {
+                                c += "0";
+                        }
+                        c += a;
+                        a = "";
+                } else {
+                        for (int i = 0; i < bada; i++) {
+                                c += "0";
+                        }
+                        c += b;
+                        b = "";
+                }
+                if (b.equals("")) {
+                        return diff(c, a);
+                } else {
+                        return diff(c, b);
+                }
         }
-    }
-    int diff(String a, String b) {
-        int cnt = 0;
-        for(int i = 0;i < a.length();i++) {
-            if(a.charAt(i)!=b.charAt(i)) cnt++;
+
+        int diff(String a, String b) {
+                int cnt = 0;
+                for (int i = 0; i < a.length(); i++) {
+                        if (a.charAt(i) != b.charAt(i))
+                                cnt++;
+                }
+                return cnt;
         }
-        return cnt;
-    }
 }
