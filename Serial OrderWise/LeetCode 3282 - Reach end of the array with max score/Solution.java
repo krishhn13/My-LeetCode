7b9f815a0c27db1aac5513adb2cffe92f2b6e193
Krishan <chiranjiv.shah783@gmail.com>
2025-12-17 14:47:37 +0530

    Released Solution for LC 3282

---
+++ Serial OrderWise/LeetCode 3282 - Reach end of the array with max score/Solution.java
@@ -0,0 +1,15 @@
+import java.util.*;
+public class Solution {
+    public long findMaximumScore(List<Integer> nums) {
+        long score = 0;
+        int prev = 0;
+        for(int i = 1;i < nums.size(); i++) {
+            if(nums.get(i) > nums.get(prev)) {
+                score = score + (long)(i -  prev) * nums.get(prev);
+                prev = i;
+            }
+        }
+        score+=(long)(nums.size()-1 - prev)*nums.get(prev);
+        return score;
+    }
+}
