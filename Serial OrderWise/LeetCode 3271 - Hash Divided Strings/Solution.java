b27d68b4eb1c1acb3e6b27be3c7df6ec2b55a055
Krishan <chiranjiv.shah783@gmail.com>
2025-12-17 12:22:56 +0530

    Released Solution for LC 3271

---
+++ Serial OrderWise/LeetCode 3271 - Hash Divided Strings/Solution.java
@@ -0,0 +1,17 @@
+public class Solution {
+    public String stringHash(String s, int k) {
+        StringBuilder sb = new StringBuilder();
+        for(int i = 0 ; i < s.length();i+=k) {
+            String str = s.substring(i,i+k);
+            sb.append((char)(97+addValues(str)));
+        }
+        return sb.toString();
+    }
+    int addValues(String str) {
+        int sum = 0;
+        for(char c : str.toCharArray()) {
+            sum+=c-'a';
+        }
+        return sum%26;
+    }
+}
