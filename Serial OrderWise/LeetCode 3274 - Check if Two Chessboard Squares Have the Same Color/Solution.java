f9f1cf1100e5133c3c40a44597af6bc80655411a
Krishan <chiranjiv.shah783@gmail.com>
2025-12-17 12:33:14 +0530

    Released Solution for LeetCode 3274

---
+++ Serial OrderWise/LeetCode 3274 - Check if Two Chessboard Squares Have the Same Color/Solution.java
@@ -0,0 +1,21 @@
+class Solution {
+    public boolean checkTwoChessboards(String c1, String c2) {
+        if(Math.abs(c1.charAt(0) - c2.charAt(0))%2 == 0) {
+            if(c1.charAt(1)%2 == 0 && c2.charAt(1)%2 == 0) {
+                return true;
+            } else if(c1.charAt(1)%2!=0 && c2.charAt(1)%2!=0) {
+                return true;
+            } else {
+                return false;
+            }
+        } else {
+            if(c1.charAt(1)%2 == 0 && c2.charAt(1)%2 != 0) {
+                return true;
+            } else if(c1.charAt(1)%2!=0 && c2.charAt(1)%2==0) {
+                return true;
+            } else {
+                return false;
+            }
+        }
+    }
+}
