b1add3b5f940375c2facdee0b305fea9718f22e6
Krishan <chiranjiv.shah783@gmail.com>
2025-12-18 19:55:39 +0530

    Released Solution for lc 881

--- Serial OrderWise/LeetCode 881 - Boats to save People/Solutionn.java
+++ Serial OrderWise/LeetCode 881 - Boats to save People/Solutionn.java
@@ -1,16 +1,17 @@
 import java.util.*;
+
 class Solution {
-    public int numRescueBoats(int[] people, int limit) {
-        Arrays.sort(people);
-        int i = 0, j = people.length - 1;
-        int boats = 0;
-        while(i <= j) {
-            if (people[i] + people[j] <= limit) {
-                i++;
-            }
-            j--; 
-            boats++;
+        public int numRescueBoats(int[] people, int limit) {
+                Arrays.sort(people);
+                int i = 0, j = people.length - 1;
+                int boats = 0;
+                while (i <= j) {
+                        if (people[i] + people[j] <= limit) {
+                                i++;
+                        }
+                        j--;
+                        boats++;
+                }
+                return boats;
         }
-        return boats;
-    }
 }
