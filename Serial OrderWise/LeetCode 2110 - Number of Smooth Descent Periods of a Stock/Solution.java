228646ff4fc75d983554fee0f0a1ae2bccb1581f
Krishan <chiranjiv.shah783@gmail.com>
2025-12-23 21:33:22 +0530

    Released Solution for LC 2110

---
+++ Serial OrderWise/LeetCode 2110 - Number of Smooth Descent Periods of a Stock/Solution.java
@@ -0,0 +1,17 @@
+
+public class Solution {
+    public long getDescentPeriods(int[] prices) {
+        long cnt =  0;
+        long n = 1;
+        for(int i = 0;i < prices.length-1;i++){
+            if(prices[i]-1 == prices[i+1]) {
+                n++;
+            } else {
+                cnt+= (n*(n+1)/2); 
+                n = 1;
+            }
+        }
+        cnt+=(n*(n+1)/2);
+        return cnt;
+    }
+}
