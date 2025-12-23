802c58a9c5688a14554e31a91b3bc24e389dfeec
Krishan <chiranjiv.shah783@gmail.com>
2025-12-23 21:32:13 +0530

    Released Solution for LC 3522

--- Serial OrderWise/LeetCode 3522 - Calculate Score After Performing Instructions/Solution.java
+++ Serial OrderWise/LeetCode 3522 - Calculate Score After Performing Instructions/Solution.java
@@ -1,20 +1,21 @@
 import java.util.*;
+
 class Solution {
-    public long calculateScore(String[] instructions, int[] values) {
-        int i = 0;
-        long score = 0;
-        Set<Integer> visited = new HashSet<>();
-        while (i >= 0 && i < instructions.length && !visited.contains(i)) {
-            visited.add(i);
-            if (instructions[i].equals("add")) {
-                score += values[i];
-                i++;
-            } else if (instructions[i].equals("jump")) {
-                i += values[i];
-            } else {
-                break;
-            }
+        public long calculateScore(String[] instructions, int[] values) {
+                int i = 0;
+                long score = 0;
+                Set<Integer> visited = new HashSet<>();
+                while (i >= 0 && i < instructions.length && !visited.contains(i)) {
+                        visited.add(i);
+                        if (instructions[i].equals("add")) {
+                                score += values[i];
+                                i++;
+                        } else if (instructions[i].equals("jump")) {
+                                i += values[i];
+                        } else {
+                                break;
+                        }
+                }
+                return score;
         }
-        return score;
-    }
 }
