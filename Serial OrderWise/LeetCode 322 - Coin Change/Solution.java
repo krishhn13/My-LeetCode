84206ccf61163fb22262a77cd85a541cb7bacddd
Krishan <chiranjiv.shah783@gmail.com>
2025-12-24 15:55:07 +0530

    Released Solution for LeetCode 322 - Coin Change

--- Serial OrderWise/LeetCode 322 - Coin Change/Solution.java
+++ Serial OrderWise/LeetCode 322 - Coin Change/Solution.java
@@ -1,29 +1,31 @@
 class Solution {
-    public int coinChange(int[] coins, int amount) {
-        int n = coins.length;
-        int[][] dp = new int[n+1][amount+1];
-        // 1st col
-        for(int i = 1;i <= n;i++) dp[i][0] = 0;
-        // 1st row
-        for(int i = 0;i <= amount; i++) dp[0][i] = ((int)1e9) - 1;
-        // 2nd row
-        for(int i = 1;i <= amount; i++) {
-            if(i%coins[0] == 0) {
-                dp[1][i] = i/coins[0];
-            } else {
-                dp[1][i] = ((int)1e9);
-            }
-        }
-        // dp solve karni h
-        for(int i = 2;i <= n; i++) {
-            for(int j = 1;j <= amount;j++) {
-                if(coins[i-1]<=j) {
-                    dp[i][j] = Math.min(1+dp[i][j-coins[i-1]] , dp[i-1][j]);
-                } else {
-                    dp[i][j] = dp[i-1][j];
+        public int coinChange(int[] coins, int amount) {
+                int n = coins.length;
+                int[][] dp = new int[n + 1][amount + 1];
+                // 1st col
+                for (int i = 1; i <= n; i++)
+                        dp[i][0] = 0;
+                // 1st row
+                for (int i = 0; i <= amount; i++)
+                        dp[0][i] = ((int) 1e9) - 1;
+                // 2nd row
+                for (int i = 1; i <= amount; i++) {
+                        if (i % coins[0] == 0) {
+                                dp[1][i] = i / coins[0];
+                        } else {
+                                dp[1][i] = ((int) 1e9);
+                        }
                 }
-            }
+                // dp solve karni h
+                for (int i = 2; i <= n; i++) {
+                        for (int j = 1; j <= amount; j++) {
+                                if (coins[i - 1] <= j) {
+                                        dp[i][j] = Math.min(1 + dp[i][j - coins[i - 1]], dp[i - 1][j]);
+                                } else {
+                                        dp[i][j] = dp[i - 1][j];
+                                }
+                        }
+                }
+                return dp[n][amount] == ((int) 1e9) ? -1 : dp[n][amount];
         }
-        return dp[n][amount] == ((int)1e9) ? -1 : dp[n][amount];
-    }
 }
