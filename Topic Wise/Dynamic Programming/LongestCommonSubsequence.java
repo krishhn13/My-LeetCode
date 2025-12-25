bb781b1ba957cf16dfd68864830af6404c5fb850
Krishan <chiranjiv.shah783@gmail.com>
2025-12-25 16:55:01 +0530

    Released Solution for LC 1143

---
+++ Topic Wise/Dynamic Programming/LongestCommonSubsequence.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int longestCommonSubsequence(String x, String y) {
+        int m = x.length() , n = y.length();
+        int[][] dp = new int[m+1][n+1];
+        for(int i = 1;i <= m; i++) {
+            for(int j = 1;j <= n; j++) {
+                if(x.charAt(i-1) == y.charAt(j-1)) {
+                    dp[i][j] = 1 + dp[i-1][j-1];
+                } else {
+                    dp[i][j] = Math.max(dp[i-1][j], dp[i][j-1]);
+                }
+            }
+        }
+        return dp[m][n];
+    }
+}
+public class LongestCommonSubsequence {
+        public static void main(String[] args) {
+                Solution sol = new Solution();
+                System.out.println(sol.longestCommonSubsequence("cyz", "hjacbjzk"));
+        }
+}
