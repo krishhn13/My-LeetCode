2a76b0f1e80604c50c56500e62fbedd5b7a56a30
Krishan <chiranjiv.shah783@gmail.com>
2025-12-28 19:40:09 +0530

    Corrected Numbering

--- Serial OrderWise/LeetCode 3761 - Minimum Absolute Distance Between Mirror Pairs/minimum-absolute-distance-between-mirror-pairs.java
+++ Serial OrderWise/LeetCode 3761 - Minimum Absolute Distance Between Mirror Pairs/minimum-absolute-distance-between-mirror-pairs.java
@@ -1,21 +1,22 @@
 class Solution {
     public int minMirrorPairDistance(int[] nums) {
-        HashMap<Integer,Integer> map = new HashMap<>();
+        HashMap<Integer, Integer> map = new HashMap<>();
         int mini = Integer.MAX_VALUE;
-        for(int i = nums.length-1;i>-1;i--) {
+        for (int i = nums.length - 1; i > -1; i--) {
             int ele = reverse(nums[i]);
-            if(map.containsKey(ele)){
-                mini = mini < map.get(ele)-i ? mini : map.get(ele)-i;
+            if (map.containsKey(ele)) {
+                mini = mini < map.get(ele) - i ? mini : map.get(ele) - i;
             }
-            map.put(nums[i],i);
+            map.put(nums[i], i);
         }
-        return mini == Integer.MAX_VALUE?-1:mini;
+        return mini == Integer.MAX_VALUE ? -1 : mini;
     }
+
     int reverse(int num) {
         int ans = 0;
-        while(num>0) {
-            ans = ans*10 + num%10;
-            num/=10;
+        while (num > 0) {
+            ans = ans * 10 + num % 10;
+            num /= 10;
         }
         return ans;
     }
