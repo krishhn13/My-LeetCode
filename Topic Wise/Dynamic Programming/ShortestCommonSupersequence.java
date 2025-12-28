960e833ac66ca0f5fd4a86792c6b7275d0be743e
Krishan <chiranjiv.shah783@gmail.com>
2025-12-28 19:34:57 +0530

    Released Solution for ShortestCommonSupersequence

---
+++ Topic Wise/Dynamic Programming/ShortestCommonSupersequence.java
@@ -0,0 +1,46 @@
+class Solution {
+        public String shortestCommonSupersequence(String s1, String s2) {
+                int m = s1.length(), n = s2.length();
+                int[][] dp = new int[m+1][n+1];
+                for(int i = 1;i <= m;i++) {
+                        for(int j = 1;j <= n;j++) {
+                                if(s1.charAt(i-1) == s2.charAt(j-1)) {
+                                        dp[i][j] = dp[i-1][j-1] + 1;
+                                } else {
+                                        dp[i][j] = Math.max(dp[i-1][j], dp[i][j-1]);
+                                }
+                        }
+                }
+                int i = m, j = n;
+                StringBuilder sb = new StringBuilder();
+                while(i>0 && j>0) {
+                        if(s1.charAt(i-1) == s2.charAt(j-1)) {
+                                sb.append(s1.charAt(i-1));
+                                i--;j--;
+                        } else {
+                                if(dp[i-1][j] > dp[i][j-1]) {
+                                        sb.append(s1.charAt(i-1));
+                                        i--;
+                                } else {
+                                        sb.append(s2.charAt(j-1));
+                                        j--;
+                                }
+                        }
+                }
+                while(i>0) {
+                        sb.append(s1.charAt(i-1));
+                        i--;
+                }
+                while(j>0) {
+                        sb.append(s2.charAt(j-1));
+                        j--;
+                }
+                return sb.reverse().toString();
+        }
+}
+public class ShortestCommonSupersequence{
+        public static void main(String[] args) {
+                Solution sol = new Solution();
+                System.out.println(sol.shortestCommonSupersequence("abcde", "dhiasode"));
+        }
+}
