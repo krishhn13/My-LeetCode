ad959a1c25bfb00a96faeade135a01daefc22efe
Krishan <chiranjiv.shah783@gmail.com>
2025-12-30 19:33:34 +0530

    Released Solution for LC 1833

--- Serial OrderWise/LeetCode 1833 - Maximum IceCream Bars/Solution.java
+++ Serial OrderWise/LeetCode 1833 - Maximum IceCream Bars/Solution.java
@@ -1,17 +1,18 @@
 import java.util.*;
+
 class Solution {
-    public int maxIceCream(int[] costs, int coins) {
-        Arrays.sort(costs);
-        int idx = 0;
-        int cnt = 0;
-        while(coins>0 && idx < costs.length) {
-            if(coins < costs[idx]) {
-                break;
-            }
-            cnt++;
-            coins-=costs[idx];
-            idx++;
+        public int maxIceCream(int[] costs, int coins) {
+                Arrays.sort(costs);
+                int idx = 0;
+                int cnt = 0;
+                while (coins > 0 && idx < costs.length) {
+                        if (coins < costs[idx]) {
+                                break;
+                        }
+                        cnt++;
+                        coins -= costs[idx];
+                        idx++;
+                }
+                return cnt;
         }
-        return cnt;
-    }
 }
