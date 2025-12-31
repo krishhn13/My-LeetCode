dc0410ec1d83e9b2d0ab4db8bc8d65469c53a404
Krishan <chiranjiv.shah783@gmail.com>
2025-12-31 14:23:29 +0530

    Released Solution for lc 25

--- Serial OrderWise/LeetCode 25 - Reverse Node in k group/Solution.java
+++ Serial OrderWise/LeetCode 25 - Reverse Node in k group/Solution.java
@@ -1,25 +1,27 @@
 class Solution {
-    public ListNode reverseKGroup(ListNode head, int k) {
-        if (head == null) return null;
-        ListNode temp = head;
-        int count = 0;
-        while (count < k) {
-            if (temp == null) return head; 
-            temp = temp.next;
-            count++;
+        public ListNode reverseKGroup(ListNode head, int k) {
+                if (head == null)
+                        return null;
+                ListNode temp = head;
+                int count = 0;
+                while (count < k) {
+                        if (temp == null)
+                                return head;
+                        temp = temp.next;
+                        count++;
+                }
+                ListNode prev = null;
+                temp = head;
+                ListNode forward = null;
+                count = 0;
+                while (count < k) {
+                        forward = temp.next;
+                        temp.next = prev;
+                        prev = temp;
+                        temp = forward;
+                        count++;
+                }
+                head.next = reverseKGroup(forward, k);
+                return prev;
         }
-        ListNode prev = null;
-        temp = head;
-        ListNode forward = null;
-        count = 0;
-        while (count < k) {
-            forward = temp.next;
-            temp.next = prev;
-            prev = temp;
-            temp = forward;
-            count++;
-        }
-        head.next = reverseKGroup(forward, k);
-        return prev;
-    }
 }
