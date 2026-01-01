e8dd7f8946f08ea7f00131d8fc43595ef5458f17
Krishan <chiranjiv.shah783@gmail.com>
2026-01-01 17:13:55 +0530

    Released sol for lc 1576

--- Serial OrderWise/LeetCode 1576 - Replace All ques. marks to Avoid Consecutive Repeating Characters/Greedy.java
+++ Serial OrderWise/LeetCode 1576 - Replace All ques. marks to Avoid Consecutive Repeating Characters/Greedy.java
@@ -1,38 +1,40 @@
 class Solution {
     public String modifyString(String s) {
-        if (s.length()==1) return "a";
+        if (s.length() == 1)
+            return "a";
         StringBuilder str = new StringBuilder();
         int n = s.length();
-        for(int i = 0;i<n;i++) {
-            if(s.charAt(i)!='?') {
+        for (int i = 0; i < n; i++) {
+            if (s.charAt(i) != '?') {
                 str.append(s.charAt(i));
             } else {
-                char a='0';
-                char b ='0';
-                if(i>0 && i<s.length()-1) {
-                    a = str.charAt(i-1);
-                    b = s.charAt(i+1);
-                } else if(i == s.length()-1) {
-                    a = str.charAt(i-1);
-                } else if(i == 0) {
-                    b = s.charAt(i+1);
+                char a = '0';
+                char b = '0';
+                if (i > 0 && i < s.length() - 1) {
+                    a = str.charAt(i - 1);
+                    b = s.charAt(i + 1);
+                } else if (i == s.length() - 1) {
+                    a = str.charAt(i - 1);
+                } else if (i == 0) {
+                    b = s.charAt(i + 1);
                 }
-                for(int ij = 0;ij < 26;ij++) {
-                    char prob = (char)(97+ij);
-                    // System.out.println(prob + " " + a + "  " + b);
-                    if(a!=prob && b!=prob) {
+                for (int ij = 0; ij < 26; ij++) {
+                    char prob = (char) (97 + ij);
+                    // System.out.println(prob + " " + a + " " + b);
+                    if (a != prob && b != prob) {
                         str.append(prob);
                         break;
-                    } 
+                    }
                 }
             }
         }
         return str.toString();
     }
 }
+
 public class Greedy {
-        public static void main(String[] args) {
-                Solution sol = new Solution();
-                System.out.println(sol.modifyString("j?qg??b"));
-        }
+    public static void main(String[] args) {
+        Solution sol = new Solution();
+        System.out.println(sol.modifyString("j?qg??b"));
+    }
 }
