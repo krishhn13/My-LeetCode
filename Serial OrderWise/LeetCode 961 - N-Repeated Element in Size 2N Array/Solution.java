0e13e68c3cb8bab789d28c3be3271abc8ccb3376
Krishan <chiranjiv.shah783@gmail.com>
2026-01-02 10:48:03 +0530

    Released Solution for LeetCode 961 - N-Repeated Element in Size 2N Array

---
+++ Serial OrderWise/LeetCode 961 - N-Repeated Element in Size 2N Array/Solution.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class Solution {
+    public int repeatedNTimes(int[] nums) {
+        Set<Integer> set= new HashSet<>();
+        for(int i : nums) {
+            if(!set.add(i)) return i;
+        }
+        return 1;
+    }
+}
