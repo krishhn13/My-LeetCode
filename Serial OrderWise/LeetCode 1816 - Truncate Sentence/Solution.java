2f8bf84dadc9d4665bae72922b68399a6a161f57
Krishan <chiranjiv.shah783@gmail.com>
2026-01-03 19:12:29 +0530

    Released Solution for LC 1816

---
+++ Serial OrderWise/LeetCode 1816 - Truncate Sentence/Solution.java
@@ -0,0 +1,12 @@
+class Solution {
+    public String truncateSentence(String s, int k) {
+        String[] arr = s.split(" ");
+        StringBuilder sb = new StringBuilder();
+        for(int i = 0; i<k;i++) {
+            String str = arr[i];
+            sb.append(str);
+            sb.append(" ");
+        }
+        return sb.toString().trim();
+    }
+}
