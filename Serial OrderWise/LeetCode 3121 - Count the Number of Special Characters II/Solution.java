c656ff64b3a5c57dbaac86b2b166466896c652de
Krishan <chiranjiv.shah783@gmail.com>
2026-01-05 17:19:30 +0530

    Released Solution for LC 3121

---
+++ Serial OrderWise/LeetCode 3121 - Count the Number of Special Characters II/Solution.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int numberOfSpecialChars(String word) {
+        Map<Character,Integer> map = new HashMap<>();
+        int idx = 0, count = 0 ;
+        for(char c : word.toCharArray()) {
+            if(Character.isUpperCase(c)) {
+                if(map.containsKey(c)) {
+                    continue;
+                } else {
+                    map.put(c, idx);
+                }
+            } else {
+                map.put(c, idx);
+            }
+            idx++;
+        }
+        for(char c = 'A'; c<='Z';c++) {
+            if(map.containsKey(c) && map.containsKey((char)(c+32))) {
+                if(map.get(c) > map.get((char)(c+32))) count++;
+            }
+        }
+        return count;
+    }
+}
