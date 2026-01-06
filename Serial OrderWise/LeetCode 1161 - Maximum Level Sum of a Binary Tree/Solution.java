717283fbb1cd8b640a84c93818fda56e7123762c
Krishan <chiranjiv.shah783@gmail.com>
2026-01-06 17:19:05 +0530

    Updated Sol for LC 398

--- Serial OrderWise/LeetCode 1161 - Maximum Level Sum of a Binary Tree/Solution.java
+++ Serial OrderWise/LeetCode 1161 - Maximum Level Sum of a Binary Tree/Solution.java
@@ -1,21 +1,25 @@
 import java.util.*;
 
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- * int val;
- * TreeNode left;
- * TreeNode right;
- * TreeNode() {}
- * TreeNode(int val) { this.val = val; }
- * TreeNode(int val, TreeNode left, TreeNode right) {
- * this.val = val;
- * this.left = left;
- * this.right = right;
- * }
- * }
- */
-class Solution {
+class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+                this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+                this.val = val;
+                this.left = left;
+                this.right = right;
+        }
+}
+
+public class Solution {
         public int maxLevelSum(TreeNode root) {
                 List<List<Integer>> list = new ArrayList<>();
                 if (root == null)
