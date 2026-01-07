a4daf078314412e97d86c7e3cff8d45113d92e81
Krishan <chiranjiv.shah783@gmail.com>
2026-01-07 21:41:54 +0530

    Released Solution for LC 79 Word Search

--- Serial OrderWise/LeetCode 79 - Word Search/Solution.java
+++ Serial OrderWise/LeetCode 79 - Word Search/Solution.java
@@ -1,14 +1,15 @@
 public class Solution {
-        boolean[][] visited; 
-        int n, m ;
+        boolean[][] visited;
+        int n, m;
+
         public boolean exist(char[][] board, String word) {
                 n = board.length;
                 m = board[0].length;
                 visited = new boolean[n][m];
-                for(int i = 0; i < n; i++) {
-                        for(int j = 0; j < m;j++) {
-                                if(board[i][j] == word.charAt(0) &&!visited[i][j]) {
-                                        if(find(board, word, 0, i, j)) {
+                for (int i = 0; i < n; i++) {
+                        for (int j = 0; j < m; j++) {
+                                if (board[i][j] == word.charAt(0) && !visited[i][j]) {
+                                        if (find(board, word, 0, i, j)) {
                                                 return true;
                                         }
                                 }
@@ -16,14 +17,14 @@ public class Solution {
                 }
                 return false;
         }
+
         private boolean find(char[][] board, String word, int i, int r, int c) {
-                if(r < 0 || r >= n || c < 0 || c >= n || visited[r][c] || board[r][c] != word.charAt(i)) {
+                if (r < 0 || r >= n || c < 0 || c >= n || visited[r][c] || board[r][c] != word.charAt(i)) {
                         return false;
                 }
                 visited[r][c] = true;
-                boolean found = (
-                        find(board, word, i+1,r+1,c) || find(board, word, i+1,r+1,c)|| find(board, word, i+1,r+1,c) || find(board, word, i+1,r+1,c)
-                );
+                boolean found = (find(board, word, i + 1, r + 1, c) || find(board, word, i + 1, r + 1, c)
+                                || find(board, word, i + 1, r + 1, c) || find(board, word, i + 1, r + 1, c));
                 visited[r][c] = false;
                 return found;
         }
