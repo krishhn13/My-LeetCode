1d29830c81f7013df6f6afee583fb273dacc0a5f
Krishan <chiranjiv.shah783@gmail.com>
2026-01-07 21:50:44 +0530

    Released Solution for LC 233 - Number of Digit one

--- Serial OrderWise/LeetCode 233 - Number of Digit One/Solution.java
+++ Serial OrderWise/LeetCode 233 - Number of Digit One/Solution.java
@@ -1,27 +1,30 @@
 import java.util.*;
+
 public class Solution {
         int[][][] dp;
-        public int countDigitOne(int n) {        
+
+        public int countDigitOne(int n) {
                 String str = String.valueOf(n);
-                dp = new int[str.length()][2][str.length()+1];
-                for(int[][] d : dp) {
-                        for(int[] r : d)  {
+                dp = new int[str.length()][2][str.length() + 1];
+                for (int[][] d : dp) {
+                        for (int[] r : d) {
                                 Arrays.fill(r, -1);
                         }
                 }
                 return solve(str, 0, 1, 0);
         }
+
         private int solve(String str, int idx, int bound, int count) {
-                if(idx == str.length()) {
+                if (idx == str.length()) {
                         return count;
                 }
                 int limit = (bound == 1) ? str.charAt(idx) - '0' : 9;
                 int ans = 0;
-                for(int i = 0; i <= limit; i++) {
-                        int updatedBound = (bound == 1 && i == limit) ? 1 : 0; 
+                for (int i = 0; i <= limit; i++) {
+                        int updatedBound = (bound == 1 && i == limit) ? 1 : 0;
                         int updatedCount = count + (i == 1 ? 1 : 0);
-                        ans += solve(str, idx+1, updatedBound, updatedCount);
-                } 
+                        ans += solve(str, idx + 1, updatedBound, updatedCount);
+                }
                 return ans;
         }
 }
