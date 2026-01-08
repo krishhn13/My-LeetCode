37d3db5eb9f125eb0d21e87ccc8ff688d341cc94
Krishan <chiranjiv.shah783@gmail.com>
2026-01-08 14:05:22 +0530

    Released Solution for LC 1458

---
+++ Serial OrderWise/LeetCode 1458 - Max Dot Product of Two Subsequences/Solution.java
@@ -0,0 +1,25 @@
+import java.util.*;
+public class Solution {
+        int[][] dp;
+        int n, m;
+        public int maxDotProduct(int[] nums1, int[] nums2) {
+                n = nums1.length;
+                m = nums2.length;
+                for(int[] d : dp) {
+                        Arrays.fill(d, -1);
+                }
+                return solve(0, 0, nums1, nums2);
+        }
+        private int solve(int i, int j, int[] nums1, int[] nums2) {
+                if(i == n || j == m) {
+                        return -(int)1e9;
+                }
+                if(dp[i][j] != -1) {
+                        return dp[i][j];
+                }
+                int takeBoth = nums1[i] * nums2[j] + Math.max(0, solve(i+1, j+1, nums1, nums2));
+                int takeOne = solve(i+1, j, nums1, nums2);
+                int takeTwo = solve(i, j+1, nums1, nums2);
+                return dp[i][j] = Math.max(takeBoth, Math.max(takeOne, takeTwo));
+        }
+}
