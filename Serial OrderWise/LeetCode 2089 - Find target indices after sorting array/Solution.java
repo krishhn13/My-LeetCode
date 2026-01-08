13db5ec0cfa7377fe19bc3fe7f05c07fdc0d2be7
Krishan <chiranjiv.shah783@gmail.com>
2026-01-08 14:27:15 +0530

    Released Solution for LC 2089

--- Serial OrderWise/LeetCode 2089 - Find target indices after sorting array/Solution.java
+++ Serial OrderWise/LeetCode 2089 - Find target indices after sorting array/Solution.java
@@ -1,11 +1,13 @@
 import java.util.*;
+
 class Solution {
-    public List<Integer> targetIndices(int[] nums, int target) {
-        List<Integer> list = new ArrayList<>();
-        Arrays.sort(nums);
-        for(int i = 0;i<nums.length;i++) {
-            if(nums[i]==target) list.add(i);
+        public List<Integer> targetIndices(int[] nums, int target) {
+                List<Integer> list = new ArrayList<>();
+                Arrays.sort(nums);
+                for (int i = 0; i < nums.length; i++) {
+                        if (nums[i] == target)
+                                list.add(i);
+                }
+                return list;
         }
-        return list;
-    }
 }
