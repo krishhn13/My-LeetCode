99f074ac09578c550b8d93477af09d0091770fbf
Krishan <chiranjiv.shah783@gmail.com>
2026-01-09 14:18:31 +0530

    Updated solution for lc 865

--- Serial OrderWise/LeetCode 865 - Smallest Subtree with all the Deepest Nodes/Solution.java
+++ Serial OrderWise/LeetCode 865 - Smallest Subtree with all the Deepest Nodes/Solution.java
@@ -1,31 +1,39 @@
 class TreeNode {
-      int val;
-      TreeNode left;
-     TreeNode right;
-      TreeNode() {}
-      TreeNode(int val) { this.val = val; }
-      TreeNode(int val, TreeNode left, TreeNode right) {
-          this.val = val;
-          this.left = left;
-          this.right = right;
-      }
- }
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode() {
+    }
+
+    TreeNode(int val) {
+        this.val = val;
+    }
+
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
 
 public class Solution {
     private int findHeight(TreeNode root) {
-        if(root == null) {
-            return  0;
+        if (root == null) {
+            return 0;
         }
-        return 1 + Math.max(findHeight(root.left), 
-                            findHeight(root.right));
+        return 1 + Math.max(findHeight(root.left),
+                findHeight(root.right));
     }
+
     public TreeNode subtreeWithAllDeepest(TreeNode root) {
-        if(root == null) return null;
+        if (root == null)
+            return null;
         int leftHeight = findHeight(root.left);
         int rightHeight = findHeight(root.right);
-        if(leftHeight > rightHeight) {
+        if (leftHeight > rightHeight) {
             return subtreeWithAllDeepest(root.left);
-        } else if(leftHeight < rightHeight) {
+        } else if (leftHeight < rightHeight) {
             return subtreeWithAllDeepest(root.right);
         } else {
             return root;
