a49746275e021a6f49ed0dc37555da308904d7a8
Krishan <chiranjiv.shah783@gmail.com>
2026-01-09 14:16:31 +0530

    Released Solution for  LC 865

---
+++ Serial OrderWise/LeetCode 865 - Smallest Subtree with all the Deepest Nodes/Solution.java
@@ -0,0 +1,36 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    private int findHeight(TreeNode root) {
+        if(root == null) {
+            return  0;
+        }
+        return 1 + Math.max(findHeight(root.left), 
+                            findHeight(root.right));
+    }
+    public TreeNode subtreeWithAllDeepest(TreeNode root) {
+        if(root == null) return null;
+        int leftHeight = findHeight(root.left);
+        int rightHeight = findHeight(root.right);
+        if(leftHeight > rightHeight) {
+            return subtreeWithAllDeepest(root.left);
+        } else if(leftHeight < rightHeight) {
+            return subtreeWithAllDeepest(root.right);
+        } else {
+            return root;
+        }
+    }
+}
