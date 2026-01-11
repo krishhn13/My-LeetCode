a6293667161d656d3a28c184b301406993bf8f0d
Krishan <chiranjiv.shah783@gmail.com>
2026-01-11 11:19:11 +0530

    Released Solution for LeetCode 3804 - Number of Centered Subarrays

--- Serial OrderWise/LeetCode 3804 - Number of Centered Subarrays/Solution.java
+++ Serial OrderWise/LeetCode 3804 - Number of Centered Subarrays/Solution.java
@@ -1,14 +1,15 @@
 import java.util.*;
+
 public class Solution {
         public int centeredSubarrays(int[] nums) {
                 int ans = 0;
-                for(int i = 0; i < nums.length; i++) {
+                for (int i = 0; i < nums.length; i++) {
                         int sum = 0;
                         Set<Integer> set = new HashSet<>();
-                        for(int j = i; j < nums.length; j++) {
-                                sum+=nums[j];
+                        for (int j = i; j < nums.length; j++) {
+                                sum += nums[j];
                                 set.add(nums[j]);
-                                if(set.contains(sum)) {
+                                if (set.contains(sum)) {
                                         ans++;
                                 }
                         }
