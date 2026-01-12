a1f8c61c1a25e7123f1dae0c78a07bc194c7ded4
Krishan <chiranjiv.shah783@gmail.com>
2026-01-12 10:21:19 +0530

    Released Soluion for LC 1390

--- Serial OrderWise/LeetCode 1390 - Four Divisors/Solution.java
+++ Serial OrderWise/LeetCode 1390 - Four Divisors/Solution.java
@@ -1,38 +1,40 @@
 class Solution {
-    public int sumFourDivisors(int[] nums) {
-        Map<Integer,Integer> freq = new HashMap<>();
-        for(int i : nums) freq.put(i, freq.getOrDefault(i, 0) + 1);
-        Map<Integer, List<Integer>> map = new HashMap<>();
-        for(int i : nums) {
-            map.putIfAbsent(i, new ArrayList<>());
-        }
-        int sum = 0;
-        for(int i : map.keySet()) {
-            map.put(i, find(i));
-            if(map.get(i).size() == 4) {
-                int l = 0;
-                for(int j : map.get(i)) {
-                    l+=j;
+        public int sumFourDivisors(int[] nums) {
+                Map<Integer, Integer> freq = new HashMap<>();
+                for (int i : nums)
+                        freq.put(i, freq.getOrDefault(i, 0) + 1);
+                Map<Integer, List<Integer>> map = new HashMap<>();
+                for (int i : nums) {
+                        map.putIfAbsent(i, new ArrayList<>());
                 }
-                sum += l*freq.get(i); 
-            }
-        }
-        return sum;
-    }
-    List<Integer> find(int num) {
-        List<Integer> list = new ArrayList<>();
-        for(int i = 1; i <= Math.sqrt(num); i++) {
-            if(num%i == 0) {
-                int d1 = i;
-                int d2 = num/i;
-                if(d1!=d2) {
-                    list.add(d1);
-                    list.add(d2);
-                } else {
-                    list.add(i);
+                int sum = 0;
+                for (int i : map.keySet()) {
+                        map.put(i, find(i));
+                        if (map.get(i).size() == 4) {
+                                int l = 0;
+                                for (int j : map.get(i)) {
+                                        l += j;
+                                }
+                                sum += l * freq.get(i);
+                        }
                 }
-            }
+                return sum;
         }
-        return list;
-    }
+
+        List<Integer> find(int num) {
+                List<Integer> list = new ArrayList<>();
+                for (int i = 1; i <= Math.sqrt(num); i++) {
+                        if (num % i == 0) {
+                                int d1 = i;
+                                int d2 = num / i;
+                                if (d1 != d2) {
+                                        list.add(d1);
+                                        list.add(d2);
+                                } else {
+                                        list.add(i);
+                                }
+                        }
+                }
+                return list;
+        }
 }
