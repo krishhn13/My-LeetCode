0ee5f1794db4a9db2de32cf96e512b3270b49329
Krishan <chiranjiv.shah783@gmail.com>
2026-01-13 19:24:53 +0530

    Released Solution for LC 3453

--- Serial OrderWise/LeetCode 3453 - Separate Squares I/Solution.java
+++ Serial OrderWise/LeetCode 3453 - Separate Squares I/Solution.java
@@ -1,32 +1,32 @@
 class Solution {
-    public double separateSquares(int[][] squares) {
-        double left = 0;
-        double right = 0;
-        for (int[] square : squares) {
-            right = Math.max(right, square[1] + square[2]);
-        }
-        while (right - left > 1e-6) {
-            double mid = (left + right) / 2;
-            double aboveArea = 0;
-            double belowArea = 0;
-            for (int[] square : squares) {
-                int y = square[1];
-                int l = square[2];
-                if (mid >= y + l) {
-                    belowArea += (double) l * l;
-                } else if (mid <= y) {
-                    aboveArea += (double) l * l;
-                } else {
-                    belowArea += (double) l * (mid - y);
-                    aboveArea += (double) l * (y + l - mid);
+        public double separateSquares(int[][] squares) {
+                double left = 0;
+                double right = 0;
+                for (int[] square : squares) {
+                        right = Math.max(right, square[1] + square[2]);
                 }
-            }
-            if (belowArea < aboveArea) {
-                left = mid;
-            } else {
-                right = mid;
-            }
+                while (right - left > 1e-6) {
+                        double mid = (left + right) / 2;
+                        double aboveArea = 0;
+                        double belowArea = 0;
+                        for (int[] square : squares) {
+                                int y = square[1];
+                                int l = square[2];
+                                if (mid >= y + l) {
+                                        belowArea += (double) l * l;
+                                } else if (mid <= y) {
+                                        aboveArea += (double) l * l;
+                                } else {
+                                        belowArea += (double) l * (mid - y);
+                                        aboveArea += (double) l * (y + l - mid);
+                                }
+                        }
+                        if (belowArea < aboveArea) {
+                                left = mid;
+                        } else {
+                                right = mid;
+                        }
+                }
+                return left;
         }
-        return left;
-    }
 }
