36010ca3fa358f82ac7b362ddf533c107d57f1d9
Krishan <chiranjiv.shah783@gmail.com>
2026-01-13 19:37:59 +0530

    Released Solution  for lc 3442

--- Serial OrderWise/LeetCode 3442 - Maximum Difference Between Even and Odd Frequency I/Solution.java
+++ Serial OrderWise/LeetCode 3442 - Maximum Difference Between Even and Odd Frequency I/Solution.java
@@ -1,21 +1,23 @@
 class Solution {
-    public int maxDifference(String s) {
-        int[] freq = new int[26];
-        for(char c : s.toCharArray()) {
-            freq[c-'a']++;
+        public int maxDifference(String s) {
+                int[] freq = new int[26];
+                for (char c : s.toCharArray()) {
+                        freq[c - 'a']++;
+                }
+                int maxOdd = -1, minOdd = (int) 1e5, maxEv = -1, minEv = (int) 1e5;
+                for (int i : freq) {
+                        if (i == 0)
+                                continue;
+                        if (i % 2 == 0) {
+                                minEv = Math.min(minEv, i);
+                                maxEv = Math.max(maxEv, i);
+                        } else {
+                                minOdd = Math.min(minOdd, i);
+                                maxOdd = Math.max(maxOdd, i);
+                        }
+                }
+                if (maxOdd == -1 || minEv == (int) 1e5)
+                        return 0;
+                return maxOdd - minEv;
         }
-        int maxOdd = -1, minOdd = (int)1e5, maxEv = -1, minEv = (int)1e5;
-        for(int i : freq) {
-            if(i == 0) continue;
-            if(i%2 == 0) {
-                minEv = Math.min(minEv, i);
-                maxEv = Math.max(maxEv, i);
-            } else {
-                minOdd = Math.min(minOdd, i);
-                maxOdd = Math.max(maxOdd, i);
-            }
-        }
-        if(maxOdd == -1 || minEv == (int)1e5) return 0;
-        return maxOdd - minEv;
-    }
 }
