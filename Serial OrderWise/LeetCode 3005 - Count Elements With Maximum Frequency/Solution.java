b2ca4c8a2857636f645a8d1b2f8575b6758ee8b0
Krishan <chiranjiv.shah783@gmail.com>
2026-01-13 19:28:53 +0530

    Released Solution for LC 3005

--- Serial OrderWise/LeetCode 3005 - Count Elements With Maximum Frequency/Solution.java
+++ Serial OrderWise/LeetCode 3005 - Count Elements With Maximum Frequency/Solution.java
@@ -1,19 +1,19 @@
 class Solution {
-    public int maxFrequencyElements(int[] nums) {
-        int[] freq = new int[101];
-        for(int i : nums) {
-            freq[i]++;
+        public int maxFrequencyElements(int[] nums) {
+                int[] freq = new int[101];
+                for (int i : nums) {
+                        freq[i]++;
+                }
+                int maxi = -1;
+                for (int i : freq) {
+                        maxi = Math.max(maxi, i);
+                }
+                int cnt = 0;
+                for (int i : freq) {
+                        if (i == maxi) {
+                                cnt++;
+                        }
+                }
+                return maxi * cnt;
         }
-        int maxi = -1;
-        for(int i : freq) {
-            maxi = Math.max(maxi, i);
-        }
-        int cnt = 0;
-        for(int i : freq) {
-            if(i == maxi) {
-                cnt++;
-            }
-        }
-        return maxi*cnt;
-    }
 }
