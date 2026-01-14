418cf77616911bb9b8ca52031173e317901f5cd1
Krishan <chiranjiv.shah783@gmail.com>
2026-01-14 10:54:19 +0530

    Released Solution for LC 1337

---
+++ Serial OrderWise/LeetCode 1337 - The K Weakest Rows in a Matrix/Solution.java
@@ -0,0 +1,36 @@
+class Pair {
+    int ones, idx;
+    Pair(int ones, int idx) {
+        this.ones = ones;
+        this.idx = idx;
+    }
+}
+public class Solution {
+    public int[] kWeakestRows(int[][] mat, int k) {
+        PriorityQueue<Pair> pq = new PriorityQueue<>(
+            (a,b) -> {
+                if(a.ones == b.ones) {
+                    return Integer.compare(a.idx, b.idx);
+                } else {
+                    return Integer.compare(a.ones, b.ones);
+                }
+            }
+        );
+        for(int i = 0; i < mat.length; i++) {
+            int cnt = 0;
+            for(int j = 0; j < mat[i].length; j++) {
+                if(mat[i][j] == 1) cnt++;
+            }
+            pq.add(new Pair(cnt, i));
+        }
+        int[] ans = new int[k];
+        int idx = 0;
+        int last = k;
+        while(last > 0) {
+            ans[idx] = pq.poll().idx;
+            idx++;
+            last--;
+        }
+        return ans;        
+    }
+}
