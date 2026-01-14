f8d14f06ef51a7d91fc98b8b991b3ac816f14688
Krishan <chiranjiv.shah783@gmail.com>
2026-01-14 11:17:59 +0530

    Released Solution for LC 384

--- Serial OrderWise/LeetCode 384 - Shuffle An Array/Solution.java
+++ Serial OrderWise/LeetCode 384 - Shuffle An Array/Solution.java
@@ -1,28 +1,31 @@
+import java.util.*;
+
 class Solution {
-    int[] arr, org;
-    int n;
-    public Solution(int[] nums) {
-        n = nums.length;
-        arr = Arrays.copyOf(nums, n);
-        org = Arrays.copyOf(nums, n);
-    }
-    
-    public int[] reset() {
-        arr = Arrays.copyOf(org, n);
-        return arr;
-    }
-    
-    public int[] shuffle() {
-        Random rand = new Random();
-        int min = 0, max = n-1;
-        for(int i = 0; i < n; i++) {
-            int idx = rand.nextInt(max - min + 1) + min;
-            int ele = arr[idx];
-            arr[idx] = arr[i];
-            arr[i] = ele;
+        int[] arr, org;
+        int n;
+
+        public Solution(int[] nums) {
+                n = nums.length;
+                org = Arrays.copyOf(nums, n);
+                arr = Arrays.copyOf(nums, n);
         }
-        return arr;
-    }
+
+        public int[] reset() {
+                arr = Arrays.copyOf(org, n);
+                return arr;
+        }
+
+        public int[] shuffle() {
+                Random rand = new Random();
+                int min = 0, max = n - 1;
+                for (int i = 0; i < n; i++) {
+                        int idx = rand.nextInt(max - min + 1) + min;
+                        int ele = arr[idx];
+                        arr[idx] = arr[i];
+                        arr[i] = ele;
+                }
+                return arr;
+        }
 }
 
 /**
