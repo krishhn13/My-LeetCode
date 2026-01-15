3248dc8fdb34c900e4b34aec4ab28e5dafee1fce
Krishan <chiranjiv.shah783@gmail.com>
2026-01-15 19:16:24 +0530

    Released Solution for Diameter of a Binary Tree

--- Serial OrderWise/LeetCode 543 - Diameter of Binary Tree/Solution.java
+++ Serial OrderWise/LeetCode 543 - Diameter of Binary Tree/Solution.java
@@ -1,18 +1,20 @@
 public class Solution {
-    int res = -(int)1e9;
-    public int diameterOfBinaryTree(TreeNode root) {
-        solve(root);
-        return res - 1;
-    }
-    private int solve(TreeNode root) {
-        if(root == null) {
-            return 0;
+        int res = -(int) 1e9;
+
+        public int diameterOfBinaryTree(TreeNode root) {
+                solve(root);
+                return res - 1;
         }
-        int l = solve(root.left);
-        int r = solve(root.right);
-        int temp = Math.max(l,r) + 1;
-        int ans = Math.max(temp, 1 + l + r);
-        res = Math.max(ans, res);
-        return temp;
-    }
+
+        private int solve(TreeNode root) {
+                if (root == null) {
+                        return 0;
+                }
+                int l = solve(root.left);
+                int r = solve(root.right);
+                int temp = Math.max(l, r) + 1;
+                int ans = Math.max(temp, 1 + l + r);
+                res = Math.max(ans, res);
+                return temp;
+        }
 }
