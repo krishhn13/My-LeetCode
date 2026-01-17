c40331c7a94e5383f80686867f749a527e1c072b
Krishan <chiranjiv.shah783@gmail.com>
2026-01-17 19:59:24 +0530

    Released Solution for LC 1909

--- Serial OrderWise/LeetCode 1909 - Remove One Element to Make the Array Strictly Increasing/Solution.java
+++ Serial OrderWise/LeetCode 1909 - Remove One Element to Make the Array Strictly Increasing/Solution.java
@@ -1,29 +1,29 @@
 class Solution {
-    public boolean canBeIncreasing(int[] nums) {
-        int removedOne = -1;
-        for(int i = 1; i < nums.length; i++) {
-            if(nums[i] - nums[i-1] > 0) {
-                if(i - 1 == removedOne) {
-                    if(i >= 2 && nums[i] - nums[i-2] < 0) {
-                        return false;
-                    }
+        public boolean canBeIncreasing(int[] nums) {
+                int removedOne = -1;
+                for (int i = 1; i < nums.length; i++) {
+                        if (nums[i] - nums[i - 1] > 0) {
+                                if (i - 1 == removedOne) {
+                                        if (i >= 2 && nums[i] - nums[i - 2] < 0) {
+                                                return false;
+                                        }
+                                }
+                        } else {
+                                if (removedOne != -1) {
+                                        return false;
+                                }
+                                if (i + 1 == nums.length)
+                                        return true;
+                                if (i + 1 < nums.length && nums[i - 1] < nums[i + 1]) {
+                                        removedOne = i;
+                                } else {
+                                        if (i >= 2 && nums[i] - nums[i - 2] <= 0) {
+                                                return false;
+                                        }
+                                        removedOne = i - 1;
+                                }
+                        }
                 }
-            } else {
-                if(removedOne != -1) {
-                    return false;
-                }
-                if(i+1 == nums.length) return true;
-                if (i+1 < nums.length && nums[i - 1] < nums[i + 1]) {
-                    removedOne = i;
-                }
-                else {
-                    if(i>=2 && nums[i] - nums[i-2] <= 0) {
-                        return false;
-                    }
-                    removedOne = i - 1;
-                }
-            }
+                return true;
         }
-        return true;
-    }
 }
