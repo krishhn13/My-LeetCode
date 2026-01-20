42b8e04779a459bebc1eb1d3c00acde50d156e68
Krishan <chiranjiv.shah783@gmail.com>
2026-01-20 13:11:51 +0530

    Released Solution for LC 3314

---
+++ Serial OrderWise/LeetCode 3314 - Construct the Minimum Bitwise Array I/Solution.java
@@ -0,0 +1,31 @@
+class Solution {
+    public int[] minBitwiseArray(List<Integer> nums) {
+        int[] ans = new int[nums.size()];
+        int idx = 0;
+        for(int i : nums) {
+            if(i == 2) {
+                ans[idx] = -1;
+            } else {
+                int num = findNext(i);
+                ans[idx] = num;
+            } 
+            idx++;
+        }
+        return ans;
+    }
+    private int findNext(int n) {
+        String bin = Integer.toBinaryString(n);
+        int cnt = 0;
+        for(int i = bin.length() - 1; i >= 0; i--) {
+            if(bin.charAt(i) == '0') {
+                break;
+            } 
+            cnt = i;
+        }
+        StringBuilder sb = new StringBuilder();
+        sb.append(bin.substring(0, cnt));
+        sb.append('0');
+        sb.append(bin.substring(cnt+1));
+        return Integer.parseInt(sb.toString(), 2);
+    }
+}
