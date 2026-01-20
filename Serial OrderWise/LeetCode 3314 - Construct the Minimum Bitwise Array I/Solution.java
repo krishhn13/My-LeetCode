2182973259bf47d8cc305e4c90117fab32c67d7a
Krishan <chiranjiv.shah783@gmail.com>
2026-01-20 13:13:42 +0530

    Released Solution for LC 3314

--- Serial OrderWise/LeetCode 3314 - Construct the Minimum Bitwise Array I/Solution.java
+++ Serial OrderWise/LeetCode 3314 - Construct the Minimum Bitwise Array I/Solution.java
@@ -1,31 +1,32 @@
 class Solution {
-    public int[] minBitwiseArray(List<Integer> nums) {
-        int[] ans = new int[nums.size()];
-        int idx = 0;
-        for(int i : nums) {
-            if(i == 2) {
-                ans[idx] = -1;
-            } else {
-                int num = findNext(i);
-                ans[idx] = num;
-            } 
-            idx++;
+        public int[] minBitwiseArray(List<Integer> nums) {
+                int[] ans = new int[nums.size()];
+                int idx = 0;
+                for (int i : nums) {
+                        if (i == 2) {
+                                ans[idx] = -1;
+                        } else {
+                                int num = findNext(i);
+                                ans[idx] = num;
+                        }
+                        idx++;
+                }
+                return ans;
         }
-        return ans;
-    }
-    private int findNext(int n) {
-        String bin = Integer.toBinaryString(n);
-        int cnt = 0;
-        for(int i = bin.length() - 1; i >= 0; i--) {
-            if(bin.charAt(i) == '0') {
-                break;
-            } 
-            cnt = i;
+
+        private int findNext(int n) {
+                String bin = Integer.toBinaryString(n);
+                int cnt = 0;
+                for (int i = bin.length() - 1; i >= 0; i--) {
+                        if (bin.charAt(i) == '0') {
+                                break;
+                        }
+                        cnt = i;
+                }
+                StringBuilder sb = new StringBuilder();
+                sb.append(bin.substring(0, cnt));
+                sb.append('0');
+                sb.append(bin.substring(cnt + 1));
+                return Integer.parseInt(sb.toString(), 2);
         }
-        StringBuilder sb = new StringBuilder();
-        sb.append(bin.substring(0, cnt));
-        sb.append('0');
-        sb.append(bin.substring(cnt+1));
-        return Integer.parseInt(sb.toString(), 2);
-    }
 }
