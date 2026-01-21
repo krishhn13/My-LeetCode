2c2c3f2ab37f4e95599a37733d9ab9098bb9bdea
Krishan <chiranjiv.shah783@gmail.com>
2026-01-21 10:14:50 +0530

    Updated Sol for 3285

--- Serial OrderWise/LeetCode 3285 - Find Indices of Stable Mountains/Solution.java
+++ Serial OrderWise/LeetCode 3285 - Find Indices of Stable Mountains/Solution.java
@@ -1,9 +1,10 @@
 import java.util.*;
+
 public class Solution {
     public List<Integer> stableMountains(int[] height, int threshold) {
         List<Integer> list = new ArrayList<>();
-        for(int i = 1 ;i < height.length;i++) {
-            if(height[i-1] > threshold) {
+        for (int i = 1; i < height.length; i++) {
+            if (height[i - 1] > threshold) {
                 list.add(i);
             }
         }
