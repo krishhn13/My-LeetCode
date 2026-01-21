20ea839fa6aa5a2f3cd0631047de1aed44ca0de2
Krishan <chiranjiv.shah783@gmail.com>
2026-01-21 10:21:56 +0530

    Updated Sol for 3760

--- Serial OrderWise/LeetCode 3760 - Maximum Substrings With Distinct Start/maximum-substrings-with-distinct-start.java
+++ Serial OrderWise/LeetCode 3760 - Maximum Substrings With Distinct Start/maximum-substrings-with-distinct-start.java
@@ -1,7 +1,8 @@
+import java.util.*;
 class Solution {
     public int maxDistinct(String s) {
         Set<Character> set = new HashSet<>();
-        for(char c : s.toCharArray()) {
+        for (char c : s.toCharArray()) {
             set.add(c);
         }
         return set.size();
