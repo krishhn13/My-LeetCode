29ab3e7ea002ccb3b171e6528281e3c4897f8938
Krishan <chiranjiv.shah783@gmail.com>
2026-01-22 12:25:23 +0530

    Released Solution for LC 3507

---
+++ Serial OrderWise/LeetCode 3507 - Minimum Pair Removal to Sort Array I/Solution.java
@@ -0,0 +1,34 @@
+class Solution {
+    int count = 0;
+    public int minimumPairRemoval(int[] nums) {
+        List<Integer> list = new ArrayList<>();
+        for(int i : nums) {
+            list.add(i);
+        }
+        while(!isSorted(list)) {
+            int x = 0;
+            int y = 1;
+            int sum = (int)1e9;
+            for(int i = 0; i < list.size()-1; i++) {
+                int curr = list.get(i) + list.get(i+1);
+                if(curr < sum) {
+                    sum = curr;
+                    x = i;
+                    y = i + 1; 
+                } 
+            }
+            list.remove(x);
+            list.set(y-1, sum);
+            count++;
+        }
+        return count;
+    }
+    private boolean isSorted(List<Integer> list) {
+        for(int i = 1; i < list.size(); i++) {
+            if(list.get(i) < list.get(i-1)) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
