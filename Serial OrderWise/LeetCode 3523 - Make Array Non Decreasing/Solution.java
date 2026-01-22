ccd746ef806244825b6cd1345e9d35b89cd3f25e
Krishan <chiranjiv.shah783@gmail.com>
2026-01-22 12:31:18 +0530

    Released Solution for LC 3523

--- Serial OrderWise/LeetCode 3523 - Make Array Non Decreasing/Solution.java
+++ Serial OrderWise/LeetCode 3523 - Make Array Non Decreasing/Solution.java
@@ -1,13 +1,13 @@
 class Solution {
-    public int maximumPossibleSize(int[] nums) {
-        int cnt=1;
-        int c = nums[0];
-        for(int i = 1;i<nums.length;i++){
-            if(c<=nums[i]){
-                cnt++;
-                c=nums[i];
-            }
+        public int maximumPossibleSize(int[] nums) {
+                int cnt = 1;
+                int c = nums[0];
+                for (int i = 1; i < nums.length; i++) {
+                        if (c <= nums[i]) {
+                                cnt++;
+                                c = nums[i];
+                        }
+                }
+                return cnt;
         }
-        return cnt;
-    }
 }
