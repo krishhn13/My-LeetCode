865f53d09b8b13868938c3b52763a443add64e64
Krishan <chiranjiv.shah783@gmail.com>
2026-01-30 15:10:33 +0530

    Released Solution for LC 1985

--- Serial OrderWise/LeetCode 1985 - Find the Kth Largest Number in an Array/Solution.java
+++ Serial OrderWise/LeetCode 1985 - Find the Kth Largest Number in an Array/Solution.java
@@ -1,12 +1,13 @@
 import java.util.*;
+
 class Solution {
-    public String kthLargestNumber(String[] nums, int k) {
-        Arrays.sort(nums, (a, b) -> {
-            if (a.length() == b.length()) {
-                return a.compareTo(b);
-            } 
-            return Integer.compare(a.length(), b.length());
-        });
-        return nums[nums.length - k];
-    }
+        public String kthLargestNumber(String[] nums, int k) {
+                Arrays.sort(nums, (a, b) -> {
+                        if (a.length() == b.length()) {
+                                return a.compareTo(b);
+                        }
+                        return Integer.compare(a.length(), b.length());
+                });
+                return nums[nums.length - k];
+        }
 }
