e7105fa6b9cac177e71334711fbfaf05aebf5121
Krishan <chiranjiv.shah783@gmail.com>
2026-01-31 20:54:31 +0530

    Released Solution for LC 179

--- Serial OrderWise/LeetCode 179 - Largest Number/Solution.java
+++ Serial OrderWise/LeetCode 179 - Largest Number/Solution.java
@@ -1,16 +1,19 @@
+import java.util.*;
+
 class Solution {
-    public String largestNumber(int[] nums) {
-        String[] s = new String[nums.length];
-        int k= 0;
-        for(int i : nums) {
-            s[k++] = String.valueOf(i);
+        public String largestNumber(int[] nums) {
+                String[] s = new String[nums.length];
+                int k = 0;
+                for (int i : nums) {
+                        s[k++] = String.valueOf(i);
+                }
+                Arrays.sort(s, (a, b) -> (b + a).compareTo(a + b));
+                if (s[0].equals("0"))
+                        return "0";
+                String ans = "";
+                for (String c : s) {
+                        ans += c;
+                }
+                return ans;
         }
-        Arrays.sort(s,(a,b)->(b+a).compareTo(a+b));
-        if(s[0].equals("0")) return  "0";
-        String ans = "";
-        for(String c : s) {
-            ans += c;
-        } 
-        return ans;
-    }
 }
