3240d9d88e73919782f97218dd0c0a8601cc041e
Krishan <chiranjiv.shah783@gmail.com>
2026-01-31 20:50:56 +0530

    Released Solution for LC 434

--- Serial OrderWise/LeetCode 434 - Number of Segments In A String/Solution.java
+++ Serial OrderWise/LeetCode 434 - Number of Segments In A String/Solution.java
@@ -1,7 +1,8 @@
 class Solution {
-    public int countSegments(String s) {
-        String[] st = s.trim().split("\\s+");
-        if(st.length == 1 && st[0].equals("")) return 0;
-        return st.length;
-    }
+        public int countSegments(String s) {
+                String[] st = s.trim().split("\\s+");
+                if (st.length == 1 && st[0].equals(""))
+                        return 0;
+                return st.length;
+        }
 }
