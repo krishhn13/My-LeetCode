d05a6f6c8f7b5d7751d0f54af07220b9846f53d0
Krishan <chiranjiv.shah783@gmail.com>
2026-01-31 20:52:32 +0530

    Released Solution for LC 3021

--- Serial OrderWise/LeetCode 3021 - Alice and Bob Playing Flower Game/Solution.java
+++ Serial OrderWise/LeetCode 3021 - Alice and Bob Playing Flower Game/Solution.java
@@ -1,5 +1,5 @@
 class Solution {
-    public long flowerGame(long n, long m) {
-        return (long)((n+1)/2)*(m/2) + ((m+1)/2)*(n/2);
-    }
+        public long flowerGame(long n, long m) {
+                return (long) ((n + 1) / 2) * (m / 2) + ((m + 1) / 2) * (n / 2);
+        }
 }
