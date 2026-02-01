91dff1e5b30586778d409dd2f212b5d150c3d802
Krishan <chiranjiv.shah783@gmail.com>
2026-02-01 14:06:58 +0530

    Released Sol for LC 1394

--- Serial OrderWise/LeetCode 1394 - Find Lucky Integer in an Array/Solution.java
+++ Serial OrderWise/LeetCode 1394 - Find Lucky Integer in an Array/Solution.java
@@ -1,13 +1,15 @@
+import java.util.*;
 class Solution {
-    public int findLucky(int[] arr) {
-        Map<Integer,Integer> map = new HashMap<>();
-        for(int i : arr){
-            map.put(i,map.getOrDefault(i,0)+1);
+        public int findLucky(int[] arr) {
+                Map<Integer, Integer> map = new HashMap<>();
+                for (int i : arr) {
+                        map.put(i, map.getOrDefault(i, 0) + 1);
+                }
+                int maxi = -1;
+                for (int i : map.keySet()) {
+                        if (map.get(i) == i && maxi < i)
+                                maxi = i;
+                }
+                return maxi;
         }
-        int maxi = -1;
-        for(int i : map.keySet()){
-            if(map.get(i) == i && maxi < i) maxi = i;
-        }
-        return maxi;
-    }
 }
