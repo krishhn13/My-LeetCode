7e572df10d419692d70eb76afb8f245abc9f171f
Krishan <chiranjiv.shah783@gmail.com>
2026-02-02 12:48:12 +0530

    Released Solution for LC 3774

--- Serial OrderWise/LeetCode 3774 - Absolute Difference Between Maximum and Minimum K Elements/Solution.java
+++ Serial OrderWise/LeetCode 3774 - Absolute Difference Between Maximum and Minimum K Elements/Solution.java
@@ -1,13 +1,14 @@
 import java.util.*;
+
 class Solution {
-    public int absDifference(int[] nums, int k) {
-        Arrays.sort(nums);
-        int s1 = 0, s2 = 0;
-        int n = nums.length;
-        for(int i = 0;i<k;i++) {
-            s1+=nums[i];
-            s2+=nums[n-i-1];
+        public int absDifference(int[] nums, int k) {
+                Arrays.sort(nums);
+                int s1 = 0, s2 = 0;
+                int n = nums.length;
+                for (int i = 0; i < k; i++) {
+                        s1 += nums[i];
+                        s2 += nums[n - i - 1];
+                }
+                return Math.abs(s1 - s2);
         }
-        return Math.abs(s1-s2);
-    }
 }
