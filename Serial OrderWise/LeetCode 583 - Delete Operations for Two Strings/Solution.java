6b5d111b0b44347f1e05b2ed04668fe4c2f9a824
Krishan <chiranjiv.shah783@gmail.com>
2026-02-02 12:45:35 +0530

    Released Sol for LC 583

--- Serial OrderWise/LeetCode 583 - Delete Operations for Two Strings/Solution.java
+++ Serial OrderWise/LeetCode 583 - Delete Operations for Two Strings/Solution.java
@@ -1,25 +1,28 @@
 import java.util.*;
+
 class Solution {
-    int[][] dp;
-    public int minDistance(String word1, String word2) {
-        int n = word1.length(), m = word2.length();
-        dp = new int[n+1][m+1];
-        for(int[] d : dp) {
-            Arrays.fill(d, -1); 
+        int[][] dp;
+
+        public int minDistance(String word1, String word2) {
+                int n = word1.length(), m = word2.length();
+                dp = new int[n + 1][m + 1];
+                for (int[] d : dp) {
+                        Arrays.fill(d, -1);
+                }
+                return n + m - 2 * solve(word1, word2, n, m);
         }
-        return n + m - 2*solve(word1,word2,n,m);
-    }
-    private int solve(String s1, String s2, int n, int m) {
-        if(n == 0 || m == 0) {
-            return 0;
+
+        private int solve(String s1, String s2, int n, int m) {
+                if (n == 0 || m == 0) {
+                        return 0;
+                }
+                if (dp[n][m] != -1) {
+                        return dp[n][m];
+                }
+                if (s1.charAt(n - 1) == s2.charAt(m - 1)) {
+                        return dp[n][m] = 1 + solve(s1, s2, n - 1, m - 1);
+                } else {
+                        return dp[n][m] = Math.max(solve(s1, s2, n - 1, m), solve(s1, s2, n, m - 1));
+                }
         }
-        if(dp[n][m] != -1) {    
-            return dp[n][m];
-        }
-        if(s1.charAt(n-1) == s2.charAt(m-1)) {
-            return dp[n][m] = 1 + solve(s1,s2,n-1, m-1);
-        } else {
-            return dp[n][m] = Math.max(solve(s1, s2, n-1, m), solve(s1, s2, n ,m-1));
-        }
-    }
 }
