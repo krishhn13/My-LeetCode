0161fa8b810edbe803f28c1f87eb2dffb978e755
Krishan <chiranjiv.shah783@gmail.com>
2026-02-03 20:18:09 +0530

    Updated code for 3058

--- Serial OrderWise/LeetCode 3058 - Maximum Number Of K Divisible Components/maximum-number-of-k-divisible-components.cpp
+++ Serial OrderWise/LeetCode 3058 - Maximum Number Of K Divisible Components/maximum-number-of-k-divisible-components.cpp
@@ -1,34 +1,40 @@
 #include <bits/stdc++.h>
 using namespace std;
-class Solution {
+class Solution
+{
 public:
-    int ans=0;
+    int ans = 0;
     vector<vector<int>> adj;
-    bitset<30001> visited=0;
-    inline void build_adj(int n, vector<vector<int>>& edges, vector<int>& values)
+    bitset<30001> visited = 0;
+    inline void build_adj(int n, vector<vector<int>> &edges, vector<int> &values)
     {
-        for(auto& e: edges){
-            int i=e[0], j=e[1];
+        for (auto &e : edges)
+        {
+            int i = e[0], j = e[1];
             adj[i].push_back(j);
             adj[j].push_back(i);
         }
     }
-    inline long long dfs(int i, vector<int>& values, int k){
-        visited[i]=1;
-        long long sum=values[i];
-        for (int j: adj[i]){
-            if (visited[j]) continue;
-            sum+=dfs(j, values, k);
-            sum%=k;
+    inline long long dfs(int i, vector<int> &values, int k)
+    {
+        visited[i] = 1;
+        long long sum = values[i];
+        for (int j : adj[i])
+        {
+            if (visited[j])
+                continue;
+            sum += dfs(j, values, k);
+            sum %= k;
         }
-        if (sum%k==0){
+        if (sum % k == 0)
+        {
             ans++;
             return 0;
         }
         return sum;
     }
-    
-    int maxKDivisibleComponents(int n, vector<vector<int>>& edges, vector<int>& values, int k) 
+
+    int maxKDivisibleComponents(int n, vector<vector<int>> &edges, vector<int> &values, int k)
     {
         adj.resize(n);
         build_adj(n, edges, values);
