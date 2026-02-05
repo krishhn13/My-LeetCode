354eaffec8badad495ce02da128ca8058e82dab0
Krishan <chiranjiv.shah783@gmail.com>
2026-02-05 20:12:31 +0530

    Released Solution for LeetCode 3146

--- Serial OrderWise/LeetCode 3146 - Permutation Difference between Two Strings/Solution.java
+++ Serial OrderWise/LeetCode 3146 - Permutation Difference between Two Strings/Solution.java
@@ -1,10 +1,10 @@
 class Solution {
-    public int findPermutationDifference(String s, String t) {
-        int sum = 0, idx = 0;
-        for(char c : s.toCharArray()) {
-            sum += Math.abs(t.indexOf(c) - idx); 
-            idx++;
+        public int findPermutationDifference(String s, String t) {
+                int sum = 0, idx = 0;
+                for (char c : s.toCharArray()) {
+                        sum += Math.abs(t.indexOf(c) - idx);
+                        idx++;
+                }
+                return sum;
         }
-        return sum;
-    }
 }
