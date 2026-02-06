54f122d0ff4d5c61a4da9242276e61ffe56baedc
Krishan <chiranjiv.shah783@gmail.com>
2026-02-06 19:25:30 +0530

    Released Solution for LeetCode 2506

--- Serial OrderWise/LeetCode 2506 - Count Pairs Of Similar Strings/Solution.java
+++ Serial OrderWise/LeetCode 2506 - Count Pairs Of Similar Strings/Solution.java
@@ -1,25 +1,27 @@
 import java.util.*;
+
 class Solution {
-    public int similarPairs(String[] words) {
-        int pairs = 0;
-        for(int i = 0; i < words.length-1; i++) {
-            for(int j = i+1; j < words.length; j++) {
-                if(count(words[i], words[j])) {
-                    pairs++;
-                } 
-            }
+        public int similarPairs(String[] words) {
+                int pairs = 0;
+                for (int i = 0; i < words.length - 1; i++) {
+                        for (int j = i + 1; j < words.length; j++) {
+                                if (count(words[i], words[j])) {
+                                        pairs++;
+                                }
+                        }
+                }
+                return pairs;
         }
-        return pairs;
-    }
-    private boolean count(String s1, String s2) {
-        Set<Character> set1 = new HashSet<>();
-        Set<Character> set2 = new HashSet<>();
-        for(char c : s1.toCharArray()) {
-            set1.add(c);
+
+        private boolean count(String s1, String s2) {
+                Set<Character> set1 = new HashSet<>();
+                Set<Character> set2 = new HashSet<>();
+                for (char c : s1.toCharArray()) {
+                        set1.add(c);
+                }
+                for (char c : s2.toCharArray()) {
+                        set2.add(c);
+                }
+                return set1.equals(set2);
         }
-        for(char c : s2.toCharArray()) {
-            set2.add(c);
-        }
-        return set1.equals(set2);
-    }
 }
