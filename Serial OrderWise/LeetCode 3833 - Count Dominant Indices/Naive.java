00c486f2c28368c8718000e3bab2d9e9003ba48f
Krishan <chiranjiv.shah783@gmail.com>
2026-02-08 15:01:58 +0530

    Released Solution for LeetCode 3833 - Count Dominant Indices

---
+++ Serial OrderWise/LeetCode 3833 - Count Dominant Indices/Naive.java
@@ -0,0 +1,5 @@
+package Serial OrderWise.LeetCode 3833 - Count Dominant Indices;
+
+public class Naive {
+        
+}
--- Serial OrderWise/LeetCode 3834 - Merge Adjacent Equal Elements/Solution.java
+++ Serial OrderWise/LeetCode 3834 - Merge Adjacent Equal Elements/Solution.java
@@ -1,24 +1,25 @@
 import java.util.*;
+
 class Solution {
-    public List<Long> mergeAdjacent(int[] nums) {
-        Stack<Long> stack = new Stack<>();
-        for(long i : nums) {
-            if(stack.isEmpty()) {
-                stack.push(i);
-            } else {
-                if(stack.peek() == i) {
-                    stack.pop();
-                    long ele = i*2;
-                    while(!stack.isEmpty() && stack.peek() == ele) {
-                        stack.pop();
-                        ele*=2;
-                    }
-                    stack.push(ele);
-                } else {
-                    stack.push(i);
+        public List<Long> mergeAdjacent(int[] nums) {
+                Stack<Long> stack = new Stack<>();
+                for (long i : nums) {
+                        if (stack.isEmpty()) {
+                                stack.push(i);
+                        } else {
+                                if (stack.peek() == i) {
+                                        stack.pop();
+                                        long ele = i * 2;
+                                        while (!stack.isEmpty() && stack.peek() == ele) {
+                                                stack.pop();
+                                                ele *= 2;
+                                        }
+                                        stack.push(ele);
+                                } else {
+                                        stack.push(i);
+                                }
+                        }
                 }
-            }
+                return new ArrayList<>(stack);
         }
-        return new ArrayList<>(stack)`;
-    }
 }
