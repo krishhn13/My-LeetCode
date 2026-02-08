f88e1770541c78bfe7daf7a05957df76f6b03c74
Krishan <chiranjiv.shah783@gmail.com>
2026-02-08 14:53:29 +0530

    Structured

--- Serial OrderWise/LeetCode 3727 - Maximum Alternating Sum of Squares/Solution.java
+++ Serial OrderWise/LeetCode 3727 - Maximum Alternating Sum of Squares/Solution.java
@@ -1,17 +1,18 @@
 import java.util.*;
+
 class Solution {
-    public long maxAlternatingSum(int[] nums) {
-        for(int i = 0; i < nums.length; i++) {
-            nums[i] = Math.abs(nums[i]);
+        public long maxAlternatingSum(int[] nums) {
+                for (int i = 0; i < nums.length; i++) {
+                        nums[i] = Math.abs(nums[i]);
+                }
+                Arrays.sort(nums);
+                long odds = 0, evens = 0;
+                for (int i = 0; i < nums.length / 2; i++) {
+                        odds += nums[i] * nums[i];
+                }
+                for (int i = nums.length / 2; i < nums.length; i++) {
+                        evens += nums[i] * nums[i];
+                }
+                return evens - odds;
         }
-        Arrays.sort(nums);
-        long odds = 0, evens = 0;
-        for(int i = 0; i < nums.length/2; i++) {
-            odds += nums[i]*nums[i];
-        }
-        for(int i = nums.length/2; i < nums.length; i++) {
-            evens += nums[i]*nums[i];
-        }
-        return evens - odds;
-    }
 }
--- Serial OrderWise/LeetCode 3741 - Minimum Distance Between Three Equal Elements II/Solution.java
+++ Serial OrderWise/LeetCode 3741 - Minimum Distance Between Three Equal Elements II/Solution.java
@@ -1,21 +1,22 @@
 import java.util.*;
+
 class Solution {
-    public int minimumDistance(int[] nums) {
-        Map<Integer, List<Integer>> map = new HashMap<>();
-        int idx = 0;
-        for (int i : nums) {
-            map.putIfAbsent(i, new ArrayList<>());
-            map.get(i).add(idx);
-            idx++;
-        }
-        int mini = Integer.MAX_VALUE;
-        for (List<Integer> list : map.values()) {
-            if (list.size() >= 3) {
-                for (int i = 0; i + 2 < list.size(); i++) {
-                    mini = Math.min(mini, 2*(list.get(i + 2) - list.get(i)));
+        public int minimumDistance(int[] nums) {
+                Map<Integer, List<Integer>> map = new HashMap<>();
+                int idx = 0;
+                for (int i : nums) {
+                        map.putIfAbsent(i, new ArrayList<>());
+                        map.get(i).add(idx);
+                        idx++;
                 }
-            }
+                int mini = Integer.MAX_VALUE;
+                for (List<Integer> list : map.values()) {
+                        if (list.size() >= 3) {
+                                for (int i = 0; i + 2 < list.size(); i++) {
+                                        mini = Math.min(mini, 2 * (list.get(i + 2) - list.get(i)));
+                                }
+                        }
+                }
+                return mini == Integer.MAX_VALUE ? -1 : mini;
         }
-        return mini == Integer.MAX_VALUE ? -1 : mini;
-    }
 }
