9a8d72948fab9a2a2542d93392cb0d0628108876
Krishan <chiranjiv.shah783@gmail.com>
2026-02-09 11:00:24 +0530

    Updated Solution for LeetCode 34

--- Serial OrderWise/LeetCode 34 - Find first and last index of an element in a sorted array/Optimised.java
+++ Serial OrderWise/LeetCode 34 - Find first and last index of an element in a sorted array/Optimised.java
@@ -1,47 +1,43 @@
 class Solution {
-        public int[] searchRange(int[] nums, int target) {
-                int first = -1, last = -1;
-                int start = 0;
-                int end = nums.length - 1;
-                int ans = -1;
-
-                // Searching starting index
-                while (start <= end) {
-                        int mid = start + (end - start) / 2;
-                        if (nums[mid] == target) {
-                                ans = mid;
-                                end = mid - 1; // Move left to find first occurrence
-                        } else if (nums[mid] > target) {
-                                end = mid - 1;
-                        } else {
-                                start = mid + 1;
-                        }
-                }
-                first = ans;
-
-                // Reset variables for second binary search
-                start = 0;
-                end = nums.length - 1;
-                ans = -1;
-
-                // Searching for the last element
-                while (start <= end) {
-                        int mid = start + (end - start) / 2;
-                        if (nums[mid] == target) {
-                                ans = mid;
-                                start = mid + 1; // Move right to find last occurrence
-                        } else if (nums[mid] > target) {
-                                end = mid - 1;
-                        } else {
-                                start = mid + 1;
-                        }
-                }
-                last = ans;
-
-                return new int[] { first, last };
+    public int[] searchRange(int[] nums, int target) {
+        int left = findLeft(nums, target); 
+        int right = findRight(nums, target); 
+        return new int[]{left, right};
+    }
+    private int findLeft(int[] nums, int target) {
+        int start = 0, end = nums.length-1;
+        int ans = -1;
+        while(start <= end) {
+            int mid = start + (end - start)/2;
+            if(nums[mid] == target) {
+                ans = mid;
+                end = mid-1;
+            } else if(nums[mid] < target) {
+                start = mid + 1;
+            } else {
+                end = mid - 1;
+            }
         }
-}
+        return ans;
+    }
+    private int findRight(int[] nums, int target) {
+        int start = 0, end = nums.length-1;
+        int ans = -1;
+        while(start <= end) {
+            int mid = start + (end - start)/2;
+            if(nums[mid] == target) {
+                ans = mid;
+                start = mid+1;
+            } else if(nums[mid] < target) {
+                start = mid + 1;
+            } else {
+                end = mid - 1;
+            }
 
+        }
+        return ans;
+    }
+}
 public class Optimised {
         public static void main(String[] args) {
                 Solution sol = new Solution();
