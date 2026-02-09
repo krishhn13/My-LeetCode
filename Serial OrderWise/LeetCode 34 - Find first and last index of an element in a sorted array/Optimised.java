b764148a00043b1b9bb16c37d483f5379f8db9c4
Krishan <chiranjiv.shah783@gmail.com>
2026-02-09 11:00:50 +0530

    Updated Solution for LC 34

--- Serial OrderWise/LeetCode 34 - Find first and last index of an element in a sorted array/Optimised.java
+++ Serial OrderWise/LeetCode 34 - Find first and last index of an element in a sorted array/Optimised.java
@@ -1,43 +1,46 @@
 class Solution {
-    public int[] searchRange(int[] nums, int target) {
-        int left = findLeft(nums, target); 
-        int right = findRight(nums, target); 
-        return new int[]{left, right};
-    }
-    private int findLeft(int[] nums, int target) {
-        int start = 0, end = nums.length-1;
-        int ans = -1;
-        while(start <= end) {
-            int mid = start + (end - start)/2;
-            if(nums[mid] == target) {
-                ans = mid;
-                end = mid-1;
-            } else if(nums[mid] < target) {
-                start = mid + 1;
-            } else {
-                end = mid - 1;
-            }
+        public int[] searchRange(int[] nums, int target) {
+                int left = findLeft(nums, target);
+                int right = findRight(nums, target);
+                return new int[] { left, right };
         }
-        return ans;
-    }
-    private int findRight(int[] nums, int target) {
-        int start = 0, end = nums.length-1;
-        int ans = -1;
-        while(start <= end) {
-            int mid = start + (end - start)/2;
-            if(nums[mid] == target) {
-                ans = mid;
-                start = mid+1;
-            } else if(nums[mid] < target) {
-                start = mid + 1;
-            } else {
-                end = mid - 1;
-            }
 
+        private int findLeft(int[] nums, int target) {
+                int start = 0, end = nums.length - 1;
+                int ans = -1;
+                while (start <= end) {
+                        int mid = start + (end - start) / 2;
+                        if (nums[mid] == target) {
+                                ans = mid;
+                                end = mid - 1;
+                        } else if (nums[mid] < target) {
+                                start = mid + 1;
+                        } else {
+                                end = mid - 1;
+                        }
+                }
+                return ans;
         }
-        return ans;
-    }
+
+        private int findRight(int[] nums, int target) {
+                int start = 0, end = nums.length - 1;
+                int ans = -1;
+                while (start <= end) {
+                        int mid = start + (end - start) / 2;
+                        if (nums[mid] == target) {
+                                ans = mid;
+                                start = mid + 1;
+                        } else if (nums[mid] < target) {
+                                start = mid + 1;
+                        } else {
+                                end = mid - 1;
+                        }
+
+                }
+                return ans;
+        }
 }
+
 public class Optimised {
         public static void main(String[] args) {
                 Solution sol = new Solution();
