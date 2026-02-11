859e750db0a2190c6637d3a6a0db58c359cd2cb9
Krishan <chiranjiv.shah783@gmail.com>
2026-02-11 20:22:12 +0530

    Released Solution for LeetCode 23

---
+++ Serial OrderWise/LeetCode 23 - Merge K Sorted Lists/Solution.java
@@ -0,0 +1,18 @@
+class Solution {
+    public ListNode mergeKLists(ListNode[] lists) {
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+        for(ListNode node : lists) {
+            while(node!=null) {
+                pq.add(node.val);
+                node = node.next;
+            }
+        }
+        ListNode dummy = new ListNode(-1);
+        ListNode curr = dummy;
+        while(!pq.isEmpty()) {
+            curr.next = new ListNode(pq.poll());
+            curr = curr.next;
+        }
+        return dummy.next;
+    }
+}
