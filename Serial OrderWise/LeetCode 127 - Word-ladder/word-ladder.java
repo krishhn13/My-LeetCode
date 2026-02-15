16e45cd7034085b16dab0cd2ac6277cb9140886a
Krishan <chiranjiv.shah783@gmail.com>
2026-02-15 19:58:55 +0530

    Updated Word Ladde 1

--- Serial OrderWise/LeetCode 127 - Word-ladder/word-ladder.java
+++ Serial OrderWise/LeetCode 127 - Word-ladder/word-ladder.java
@@ -1,5 +1,6 @@
 import java.util.*;
-class Pair{
+
+class Pair {
     String word;
     int level;
     Pair(String word, int level) {
@@ -7,30 +8,34 @@ class Pair{
         this.level = level;
     }
 }
+
 class Solution {
     public int ladderLength(String beginWord, String endWord, List<String> wordList) {
         Set<String> set = new HashSet<>();
-        for(String s : wordList) {
+        for (String s : wordList) {
             set.add(s);
         }
-        if(!set.contains(endWord)) return 0;
+        if (!set.contains(endWord))
+            return 0;
         Queue<Pair> q = new LinkedList<>();
-        if(set.contains(beginWord)) set.remove(beginWord);
-        q.add(new Pair(beginWord,1));
+        if (set.contains(beginWord))
+            set.remove(beginWord);
+        q.add(new Pair(beginWord, 1));
         int ans = 0;
-        while(!q.isEmpty()) {
+        while (!q.isEmpty()) {
             Pair p = q.poll();
             String word = p.word;
             int level = p.level;
-            if(word.equals(endWord)) return level;
-            for(int i = 0;i < word.length();i++) {
-                for(char c = 'a';c<='z';c++) {
+            if (word.equals(endWord))
+                return level;
+            for (int i = 0; i < word.length(); i++) {
+                for (char c = 'a'; c <= 'z'; c++) {
                     char[] charArray = word.toCharArray();
                     charArray[i] = c;
                     String prob = new String(charArray);
-                    if(set.contains(prob)) {
+                    if (set.contains(prob)) {
                         set.remove(prob);
-                        q.add(new Pair(prob,level+1));
+                        q.add(new Pair(prob, level + 1));
                     }
                 }
             }
