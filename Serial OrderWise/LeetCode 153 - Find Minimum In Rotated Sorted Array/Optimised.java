608bee23f294a6fb1bf2f88aca5c757047d967ac
Krishan <chiranjiv.shah783@gmail.com>
2026-02-15 20:44:42 +0530

    Updated Find minimum in rotated sorted array

--- Serial OrderWise/LeetCode 153 - Find Minimum In Rotated Sorted Array/Optimised.java
+++ Serial OrderWise/LeetCode 153 - Find Minimum In Rotated Sorted Array/Optimised.java
@@ -1,10 +1,10 @@
-class Solution{
+class Solution {
         public int findMin(int[] arr) {
-                int left = 0, right = arr.length-1;
-                while (left<right) {
-                        int mid = left + (right-left)/2;
-                        if(arr[mid]>arr[right]) {
-                                left = mid+1;
+                int left = 0, right = arr.length - 1;
+                while (left < right) {
+                        int mid = left + (right - left) / 2;
+                        if (arr[mid] > arr[right]) {
+                                left = mid + 1;
                         } else {
                                 right = mid;
                         }
@@ -12,10 +12,11 @@ class Solution{
                 return arr[left];
         }
 }
+
 public class Optimised {
         public static void main(String[] args) {
                 Solution sol = new Solution();
-                int[] arr = {3,4,5,1,2};
+                int[] arr = { 3, 4, 5, 1, 2 };
                 System.out.println(sol.findMin(arr));
         }
 }
