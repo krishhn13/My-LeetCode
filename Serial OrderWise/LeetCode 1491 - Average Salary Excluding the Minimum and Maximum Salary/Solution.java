4bdb1e1c2295fb574ae229018a3c9ccaeb98855b
Krishan <chiranjiv.shah783@gmail.com>
2026-02-16 20:04:33 +0530

    Released Solution for LC 1491

--- Serial OrderWise/LeetCode 1491 - Average Salary Excluding the Minimum and Maximum Salary/Solution.java
+++ Serial OrderWise/LeetCode 1491 - Average Salary Excluding the Minimum and Maximum Salary/Solution.java
@@ -1,19 +1,19 @@
 class Solution {
-    public double average(int[] salary) {
-        int maxi = Integer.MIN_VALUE;
-        int mini = Integer.MAX_VALUE;
-        for(int i  : salary){
-            maxi = maxi > i ? maxi : i;
-            mini = mini > i ? i : mini;
+        public double average(int[] salary) {
+                int maxi = Integer.MIN_VALUE;
+                int mini = Integer.MAX_VALUE;
+                for (int i : salary) {
+                        maxi = maxi > i ? maxi : i;
+                        mini = mini > i ? i : mini;
+                }
+                int count = 0;
+                double sum = 0;
+                for (int i : salary) {
+                        if (i != maxi && i != mini) {
+                                sum += i;
+                                count++;
+                        }
+                }
+                return sum / count;
         }
-        int count = 0;
-        double sum = 0;
-        for(int i : salary){
-            if(i!=maxi && i!=mini) {
-                sum+=i;
-                count++;
-            }    
-        }
-        return sum/count;
-    }
 }
