0eabc7c6b8eadbf2303ba4e5926a211f66453802
Krishan <chiranjiv.shah783@gmail.com>
2026-02-17 20:28:57 +0530

    Updated LC 216

---
+++ Serial OrderWise/LeetCode 216 - Combination Sum III/Solution.java
@@ -0,0 +1,18 @@
+class Solution {
+    public List<List<Integer>> combinationSum3(int k, int n) {
+        List<List<Integer>> list = new ArrayList<>();
+        generate(list, k, n, 1, new ArrayList<>());
+        return list;
+    }
+    private void generate(List<List<Integer>> list, int k, int n, int idx, List<Integer> l) {
+        if(l.size() == k && n == 0) {
+            list.add(new ArrayList<>(l));
+            return ;
+        }
+        for(int i = idx; i <= 9; i++) {
+            l.add(i);
+            generate(list, k, n - i, i+1, l);
+            l.remove(l.size()-1);
+        }
+    }
+}
