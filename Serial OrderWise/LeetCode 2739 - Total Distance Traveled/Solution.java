c2310401c450e531d22ce7b7b238846689dc80d5
Krishan <chiranjiv.shah783@gmail.com>
2026-02-18 15:38:56 +0530

    Released Solution for LeetCode 2739

--- Serial OrderWise/LeetCode 2739 - Total Distance Traveled/Solution.java
+++ Serial OrderWise/LeetCode 2739 - Total Distance Traveled/Solution.java
@@ -1,14 +1,14 @@
 class Solution {
-    public int distanceTraveled(int mainTank, int additionalTank) {
-        int ans = 0;
-        while(mainTank>=5 && additionalTank>0){
-            ans+=50;
-            mainTank-=4;
-            additionalTank-=1;
+        public int distanceTraveled(int mainTank, int additionalTank) {
+                int ans = 0;
+                while (mainTank >= 5 && additionalTank > 0) {
+                        ans += 50;
+                        mainTank -= 4;
+                        additionalTank -= 1;
+                }
+                if (mainTank > 0) {
+                        ans += mainTank * 10;
+                }
+                return ans;
         }
-        if(mainTank>0){
-            ans+=mainTank*10;
-        }
-        return ans;
-    }
 }
