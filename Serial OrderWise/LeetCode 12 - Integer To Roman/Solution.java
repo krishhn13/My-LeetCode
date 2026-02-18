d315d95a8d770be3b9dc8d08c3d4d785d058f349
Krishan <chiranjiv.shah783@gmail.com>
2026-02-18 15:40:18 +0530

    Released Solution for LeetCode 12

--- Serial OrderWise/LeetCode 12 - Integer To Roman/Solution.java
+++ Serial OrderWise/LeetCode 12 - Integer To Roman/Solution.java
@@ -1,19 +1,19 @@
 class Solution {
-    public String intToRoman(int num) {
-        final int[] values = {1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};
-        final String[] symbols = {"M", "CM", "D",  "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"};
+        public String intToRoman(int num) {
+                final int[] values = { 1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1 };
+                final String[] symbols = { "M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I" };
 
-        StringBuilder sb = new StringBuilder();
+                StringBuilder sb = new StringBuilder();
 
-        for (int i = 0; i < values.length; ++i) {
-            if (num == 0)
-                break;
-            while (num >= values[i]) {
-                sb.append(symbols[i]);
-                num -= values[i];
-            }
+                for (int i = 0; i < values.length; ++i) {
+                        if (num == 0)
+                                break;
+                        while (num >= values[i]) {
+                                sb.append(symbols[i]);
+                                num -= values[i];
+                        }
+                }
+
+                return sb.toString();
         }
-
-        return sb.toString();        
-    }
 }
