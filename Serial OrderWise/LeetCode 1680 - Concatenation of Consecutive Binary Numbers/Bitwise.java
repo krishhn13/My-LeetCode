2d03c78a210c211f4ef802bc0100fdb4b0fb0d44
Krishan <chiranjiv.shah783@gmail.com>
2026-02-19 20:01:05 +0530

    Released solution for LC 1680

---
+++ Serial OrderWise/LeetCode 1680 - Concatenation of Consecutive Binary Numbers/Bitwise.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int concatenatedBinary(int n) {
+        long ans = 0;
+        int mod = (int)1e9 + 7;
+        int len = 0;
+        for(int i = 1;i<=n;i++) {
+            if((i&(i-1))==0) {
+                len++;
+            }
+            ans = ((ans<<len)+i)%mod;
+        }
+        return (int)ans;
+    }
+}
