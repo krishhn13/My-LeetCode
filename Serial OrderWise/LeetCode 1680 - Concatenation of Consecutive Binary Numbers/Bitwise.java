558b33c64e2a1e7d93b4708f6fdf9e5529cda360
Krishan <chiranjiv.shah783@gmail.com>
2026-02-19 20:05:10 +0530

    Updated 1680

--- Serial OrderWise/LeetCode 1680 - Concatenation of Consecutive Binary Numbers/Bitwise.java
+++ Serial OrderWise/LeetCode 1680 - Concatenation of Consecutive Binary Numbers/Bitwise.java
@@ -1,16 +1,16 @@
 class Solution {
-    public int concatenatedBinary(int n) {
-        long ans = 0;
-        int mod = (int)1e9 + 7;
-        int len = 0;
-        for(int i = 1;i<=n;i++) {
-            if((i&(i-1))==0) {
-                len++;
-            }
-            ans = ((ans<<len)+i)%mod;
+        public int concatenatedBinary(int n) {
+                long ans = 0;
+                int mod = (int) 1e9 + 7;
+                int len = 0;
+                for (int i = 1; i <= n; i++) {
+                        if ((i & (i - 1)) == 0) {
+                                len++;
+                        }
+                        ans = ((ans << len) + i) % mod;
+                }
+                return (int) ans;
         }
-        return (int)ans;
-    }
 }
 
 public class Bitwise {
