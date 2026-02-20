df87ca2272e75f4fdd87e46fb0eb11935ff0acf0
Krishan <chiranjiv.shah783@gmail.com>
2026-02-20 20:17:49 +0530

    Released Solution for 2353

---
+++ Serial OrderWise/LeetCode 2353 - Design a Food Rating System/Solution.java
@@ -0,0 +1,37 @@
+import java.util.*;
+class FoodRatings {
+    Map<String, String> foodToCuisine = new HashMap<>();
+    Map<String, Integer> foodToRating = new HashMap<>();
+    Map<String, TreeSet<String>> cuisineToFoods = new HashMap<>();
+
+    public FoodRatings(String[] foods, String[] cuisines, int[] ratings) {
+        for (int i = 0; i < foods.length; i++) {
+            String f = foods[i];
+            String c = cuisines[i];
+            int r = ratings[i];
+
+            foodToCuisine.put(f, c);
+            foodToRating.put(f, r);
+
+            cuisineToFoods.putIfAbsent(c, new TreeSet<>((a, b) -> {
+                int ra = foodToRating.get(a);
+                int rb = foodToRating.get(b);
+                if (ra != rb) return rb - ra; 
+                return a.compareTo(b);       
+            }));
+            cuisineToFoods.get(c).add(f);
+        }
+    }
+
+    public void changeRating(String f, int newRating) {
+        String c = foodToCuisine.get(f);
+        TreeSet<String> set = cuisineToFoods.get(c);
+        set.remove(f);
+        foodToRating.put(f, newRating);
+        set.add(f);
+    }
+
+    public String highestRated(String c) {
+        return cuisineToFoods.get(c).first();
+    }
+}
