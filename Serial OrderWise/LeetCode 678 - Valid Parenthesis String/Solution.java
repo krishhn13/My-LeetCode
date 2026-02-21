dff93ea0b1b951c68fa9e81b62a842afd87cb2a0
Krishan <chiranjiv.shah783@gmail.com>
2026-02-21 21:45:45 +0530

    Released Sol for LeetCode 678 - Valid Parenthesis String

--- Serial OrderWise/LeetCode 678 - Valid Parenthesis String/Solution.java
+++ Serial OrderWise/LeetCode 678 - Valid Parenthesis String/Solution.java
@@ -1,28 +1,28 @@
 class Solution {
-    public boolean checkValidString(String s) {
-        Stack<Integer> leftParen = new Stack<>();
-        Stack<Integer> star = new Stack<>();
-        for (int i = 0; i < s.length(); i++) {
-            char c = s.charAt(i);
-            if (c == '(') {
-                leftParen.push(i);
-            } else if (c == '*') {
-                star.push(i);
-            } else if (c == ')') {
-                if (!leftParen.isEmpty()) {
-                    leftParen.pop();
-                } else if (!star.isEmpty()) {
-                    star.pop();
-                } else {
-                    return false;
+        public boolean checkValidString(String s) {
+                Stack<Integer> leftParen = new Stack<>();
+                Stack<Integer> star = new Stack<>();
+                for (int i = 0; i < s.length(); i++) {
+                        char c = s.charAt(i);
+                        if (c == '(') {
+                                leftParen.push(i);
+                        } else if (c == '*') {
+                                star.push(i);
+                        } else if (c == ')') {
+                                if (!leftParen.isEmpty()) {
+                                        leftParen.pop();
+                                } else if (!star.isEmpty()) {
+                                        star.pop();
+                                } else {
+                                        return false;
+                                }
+                        }
                 }
-            }
+                while (!leftParen.isEmpty() && !star.isEmpty()) {
+                        if (leftParen.pop() > star.pop()) {
+                                return false;
+                        }
+                }
+                return leftParen.isEmpty();
         }
-        while (!leftParen.isEmpty() && !star.isEmpty()) {
-            if (leftParen.pop() > star.pop()) {
-                return false;
-            }
-        }
-        return leftParen.isEmpty();
-    }
 }
