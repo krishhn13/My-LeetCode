c5747d5a69cf94565e71b30a7d675197705b7639
Krishan <chiranjiv.shah783@gmail.com>
2026-02-22 21:57:04 +0530

    Released Solution for LC 868

---
+++ Serial OrderWise/LeetCode 868 - Binary Gap/Solution.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int binaryGap(int n) {
+        if(Integer.bitCount(n) == 1) return 0;
+        String val = Integer.toBinaryString(n);
+        int maxi = 0;
+        int curr = 0;
+        boolean seenOne = false;
+        for(char c : val.toCharArray()) {
+            if(c == '1') {
+                if(seenOne) {
+                    maxi = Math.max(maxi, curr);
+                }
+                seenOne = true;
+                curr = 0;
+            } 
+            else if(seenOne) {
+                curr++;
+            }
+        }
+        return maxi+1;
+    }
+}
