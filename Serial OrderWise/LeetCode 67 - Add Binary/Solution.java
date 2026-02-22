7f2aa384574017b509ba69d9b3d27605c2df4d95
Krishan <chiranjiv.shah783@gmail.com>
2026-02-22 21:59:01 +0530

    Released Solution for LC 67

--- Serial OrderWise/LeetCode 67 - Add Binary/Solution.java
+++ Serial OrderWise/LeetCode 67 - Add Binary/Solution.java
@@ -1,21 +1,21 @@
 class Solution {
-    public String addBinary(String a, String b) {
-        int carry = 0;
-        StringBuilder str = new StringBuilder();
-        int i = a.length()-1, j = b.length()-1;
-        while(i>=0 || j>=0 || carry != 0) {
-            int sum = carry;
-            if(i>=0) {
-                sum += a.charAt(i)-'0';
-                i--;
-            }
-            if(j>=0) {
-                sum += b.charAt(j)-'0';
-                j--;
-            }
-            str.append(sum%2);
-            carry = sum/2;
+        public String addBinary(String a, String b) {
+                int carry = 0;
+                StringBuilder str = new StringBuilder();
+                int i = a.length() - 1, j = b.length() - 1;
+                while (i >= 0 || j >= 0 || carry != 0) {
+                        int sum = carry;
+                        if (i >= 0) {
+                                sum += a.charAt(i) - '0';
+                                i--;
+                        }
+                        if (j >= 0) {
+                                sum += b.charAt(j) - '0';
+                                j--;
+                        }
+                        str.append(sum % 2);
+                        carry = sum / 2;
+                }
+                return str.reverse().toString();
         }
-        return str.reverse().toString();
-    }
 }
