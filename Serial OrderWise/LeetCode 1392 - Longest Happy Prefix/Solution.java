f0bdf418e60b4aba5c98cd947ab32531f70dc7cb
Krishan <chiranjiv.shah783@gmail.com>
2026-02-23 15:23:24 +0530

    Released Solution for LeetCode 1392

---
+++ Serial OrderWise/LeetCode 1392 - Longest Happy Prefix/Solution.java
@@ -0,0 +1,22 @@
+class Solution {
+    public String longestPrefix(String s) {
+        int last = build(s);
+        return s.substring(0,last);   
+    }
+    private int build(String str) {
+        int[] lps = new int[str.length()];
+        int i = 1, len = 0;
+        while(i<str.length()) {
+            if(str.charAt(i) == str.charAt(len)) {
+                len++;
+                lps[i] = len;
+                i++;
+            } else if(len>0) {
+                len = lps[len-1];
+            } else {
+                i++;
+            }
+        } 
+        return lps[lps.length-1];
+    }
+}
