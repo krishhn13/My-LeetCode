d2cc07ebdb0ff66bc7f32538116be2c852d61d4e
Krishan <chiranjiv.shah783@gmail.com>
2026-02-25 21:38:38 +0530

    Update

--- Serial OrderWise/LeetCode 3264 -  Final Array State After K Multiplication Operations I/Solution.java
+++ Serial OrderWise/LeetCode 3264 -  Final Array State After K Multiplication Operations I/Solution.java
@@ -2,14 +2,14 @@ class Solution {
         public int[] getFinalState(int[] nums, int k, int multiplier) {
                 while (k > 0) {
                         int mini = nums[0];
-                        int index = 0;
+                        int idx = 0;
                         for (int i = 0; i < nums.length; i++) {
                                 if (mini > nums[i]) {
                                         mini = nums[i];
-                                        index = i;
+                                        idx = i;
                                 }
                         }
-                        nums[index] = mini * multiplier;
+                        nums[idx] = mini * multiplier;
                         k--;
                 }
                 return nums;
