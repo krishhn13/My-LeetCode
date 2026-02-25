f23ea761b8819049406ec9b04955938333bd5a27
Krishan <chiranjiv.shah783@gmail.com>
2026-02-25 15:00:50 +0530

    Released Solution for 1356

---
+++ Serial OrderWise/LeetCode 1356 - Sort Integers by The Number of 1 Bits/Solution.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int[] sortByBits(int[] arr) {
+        Integer[] ans = new Integer[arr.length];
+        for(int i = 0; i < ans.length; i++) {
+            ans[i] = arr[i];
+        }
+        Arrays.sort(ans,(a,b)->{
+            int cmp = Integer.compare(Integer.bitCount(a), Integer.bitCount(b));
+            if(cmp == 0) return Integer.compare(a, b);
+            return cmp;
+        });
+        for(int i = 0; i < ans.length; i++) {
+            arr[i] = ans[i];
+        }
+        return arr ;
+    }
+}
