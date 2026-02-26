294e6c0df4bb1f0f80846609a99f6bd221bd1ce7
Krishan <chiranjiv.shah783@gmail.com>
2026-02-26 20:46:11 +0530

    Released Solution for LC 2078

---
+++ Serial OrderWise/LeetCode 2078 - Two Furthest Houses With Different Colors/Solution.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int maxDistance(int[] colors) {
+        int l = 0 , r = colors.length-1;
+        while(r>l) {
+            if(colors[l]!=colors[r]) {
+                break;
+            }
+            r--;
+        }
+        int left = 0 , right = colors.length-1;
+        while(left < right) {
+            if(colors[left]!=colors[right]) break;
+            left++;
+        }
+        return Math.max(r-l,right-left);
+    }
+}
