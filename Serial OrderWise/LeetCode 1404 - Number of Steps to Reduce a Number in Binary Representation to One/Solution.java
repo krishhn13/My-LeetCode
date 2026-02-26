2996dfd90899936f3220fb78bfd73d1d40b53cdf
Krishan <chiranjiv.shah783@gmail.com>
2026-02-26 20:43:57 +0530

    Released Solution for LC 1404

--- Serial OrderWise/LeetCode 1404 - Number of Steps to Reduce a Number in Binary Representation to One/Solution.java
+++ Serial OrderWise/LeetCode 1404 - Number of Steps to Reduce a Number in Binary Representation to One/Solution.java
@@ -1,15 +1,15 @@
 class Solution {
-    public int numSteps(String s) {
-        int cnt = 0, carry = 0;
-        for(int i = s.length()-1; i>0; i--) {
-            int bit = (s.charAt(i)-'0')+carry;
-            if(bit%2 == 0) {
-                cnt++;
-            } else {
-                cnt+=2;
-                carry = 1;
-            }
+        public int numSteps(String s) {
+                int cnt = 0, carry = 0;
+                for (int i = s.length() - 1; i > 0; i--) {
+                        int bit = (s.charAt(i) - '0') + carry;
+                        if (bit % 2 == 0) {
+                                cnt++;
+                        } else {
+                                cnt += 2;
+                                carry = 1;
+                        }
+                }
+                return cnt + carry;
         }
-        return cnt+carry;
-    }
 }
