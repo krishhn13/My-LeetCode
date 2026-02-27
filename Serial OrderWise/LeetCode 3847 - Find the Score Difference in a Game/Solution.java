517c4c87bf7988d1aedb6e8a6df3493fd2f73403
Krishan <chiranjiv.shah783@gmail.com>
2026-02-27 15:48:11 +0530

    Released Solution for LeetCode3847

--- Serial OrderWise/LeetCode 3847 - Find the Score Difference in a Game/Solution.java
+++ Serial OrderWise/LeetCode 3847 - Find the Score Difference in a Game/Solution.java
@@ -1,20 +1,20 @@
 class Solution {
-    public int scoreDifference(int[] nums) {
-        int s1 = 0, s2 = 0;
-        boolean p1 = true;
-        for(int i = 0; i < nums.length; i++) {
-            if(nums[i] % 2 != 0) {
-                p1 = !p1;
-            }
-            if((i+1)%6==0) {
-                p1 = !p1;
-            }
-            if(p1) {
-                s1 += nums[i];
-            } else {
-                s2 += nums[i];
-            }
+        public int scoreDifference(int[] nums) {
+                int s1 = 0, s2 = 0;
+                boolean p1 = true;
+                for (int i = 0; i < nums.length; i++) {
+                        if (nums[i] % 2 != 0) {
+                                p1 = !p1;
+                        }
+                        if ((i + 1) % 6 == 0) {
+                                p1 = !p1;
+                        }
+                        if (p1) {
+                                s1 += nums[i];
+                        } else {
+                                s2 += nums[i];
+                        }
+                }
+                return s1 - s2;
         }
-        return s1 - s2;
-    }
 }
