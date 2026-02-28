1b2ca3326e24e0929a11a289150b4029329f2b0e
Krishan <chiranjiv.shah783@gmail.com>
2026-02-28 20:44:11 +0530

    Released Sol for LC 117

---
+++ Serial OrderWise/LeetCode 117 - Populating Next Right Pointers in Each Node II/Solution.java
@@ -0,0 +1,23 @@
+class Solution {
+    public Node connect(Node root) {
+        if (root == null) return null;
+        Node curr = root;
+        while (curr != null) {
+            Node dummy = new Node(0);
+            Node tail = dummy;
+            while (curr != null) {
+                if (curr.left != null) {
+                    tail.next = curr.left;
+                    tail = tail.next;
+                }
+                if (curr.right != null) {
+                    tail.next = curr.right;
+                    tail = tail.next;
+                }
+                curr = curr.next;
+            }
+            curr = dummy.next;
+        }
+        return root;
+    }
+}
