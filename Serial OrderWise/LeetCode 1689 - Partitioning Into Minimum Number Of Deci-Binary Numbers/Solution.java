9f6dacede8db55936eae874ed4e0c691a5bccaaa
Krishan <chiranjiv.shah783@gmail.com>
2026-03-01 17:55:30 +0530

    Released Solution For LeetCode 1689

--- Serial OrderWise/LeetCode 1689 - Partitioning Into Minimum Number Of Deci-Binary Numbers/Solution.java
+++ Serial OrderWise/LeetCode 1689 - Partitioning Into Minimum Number Of Deci-Binary Numbers/Solution.java
@@ -1,9 +1,9 @@
 class Solution {
-    public int minPartitions(String n) {
-        int maxi = 0;
-        for(char c : n.toCharArray()) {
-            maxi = Math.max(c-'0', maxi);
+        public int minPartitions(String n) {
+                int maxi = 0;
+                for (char c : n.toCharArray()) {
+                        maxi = Math.max(c - '0', maxi);
+                }
+                return maxi;
         }
-        return maxi;
-    }
 }
