b1e2a9d952635af9bbba2dad1da60885cd670b3b
Krishan <chiranjiv.shah783@gmail.com>
2026-03-03 19:58:36 +0530

    Released Solution for LC 3622

--- Serial OrderWise/LeetCode 3622 - Check Divisibility by Digit Sum and Product/Solution.java
+++ Serial OrderWise/LeetCode 3622 - Check Divisibility by Digit Sum and Product/Solution.java
@@ -1,23 +1,25 @@
 class Solution {
-    public boolean checkDivisibility(int n) {
-        int prod = prod(n);
-        int sum = sum(n);
-        return n%(sum+prod) == 0;
-    }
-    int prod(int n) {
-        int ans = 1;
-        while(n>0) {
-            ans*=n%10;
-            n/=10;
+        public boolean checkDivisibility(int n) {
+                int prod = prod(n);
+                int sum = sum(n);
+                return n % (sum + prod) == 0;
         }
-        return ans;
-    }
-    int sum(int n) {
-        int ans = 0;
-        while(n>0) {
-            ans += n%10;
-            n/=10;
+
+        int prod(int n) {
+                int ans = 1;
+                while (n > 0) {
+                        ans *= n % 10;
+                        n /= 10;
+                }
+                return ans;
         }
-        return ans;
-    }
+
+        int sum(int n) {
+                int ans = 0;
+                while (n > 0) {
+                        ans += n % 10;
+                        n /= 10;
+                }
+                return ans;
+        }
 }
