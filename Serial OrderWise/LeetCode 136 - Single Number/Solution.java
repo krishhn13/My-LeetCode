6bbc77d4fef86473a96f78fed60c847e81224e9b
Krishan <chiranjiv.shah783@gmail.com>
2026-03-04 16:28:20 +0530

    Released Solution for LeetCode 136

--- Serial OrderWise/LeetCode 136 - Single Number/Solution.java
+++ Serial OrderWise/LeetCode 136 - Single Number/Solution.java
@@ -1,9 +1,9 @@
 class Solution {
-    public int singleNumber(int[] nums) {
-        int ans = 0;
-        for(int i :nums){
-            ans^=i;
+        public int singleNumber(int[] nums) {
+                int ans = 0;
+                for (int i : nums) {
+                        ans ^= i;
+                }
+                return ans;
         }
-        return ans;
-    }
 }
