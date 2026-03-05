c82c36db982f95f6d409d35fdb5856e4e556f3df
Krishan <chiranjiv.shah783@gmail.com>
2026-03-05 20:23:24 +0530

    Released Solution for LC 1758

--- Serial OrderWise/LeetCode 1758 - Minimum Changes To Make Alternating Binary String/Solution.java
+++ Serial OrderWise/LeetCode 1758 - Minimum Changes To Make Alternating Binary String/Solution.java
@@ -1,12 +1,12 @@
 class Solution {
-    public int minOperations(String s) {
-        int count = 0;
-        for(int i = 0;i < s.length();i++){
-            char ch = (i%2==0)?'0':'1';
-            if(s.charAt(i)!=ch) {
-                count++;
-            }
+        public int minOperations(String s) {
+                int count = 0;
+                for (int i = 0; i < s.length(); i++) {
+                        char ch = (i % 2 == 0) ? '0' : '1';
+                        if (s.charAt(i) != ch) {
+                                count++;
+                        }
+                }
+                return Math.min(count, s.length() - count);
         }
-        return Math.min(count,s.length()-count);
-    }
 }
