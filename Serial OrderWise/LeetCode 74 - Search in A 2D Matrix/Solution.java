21382c7b2f30cd46df9536119b518ec6f75f66e8
Krishan <chiranjiv.shah783@gmail.com>
2026-03-05 20:25:34 +0530

    Released Solution for LC 74

--- Serial OrderWise/LeetCode 74 - Search in A 2D Matrix/Solution.java
+++ Serial OrderWise/LeetCode 74 - Search in A 2D Matrix/Solution.java
@@ -3,27 +3,22 @@ class Solution {
                 int rows = arr.length;
                 if (rows == 0)
                         return false;
-
                 int cols = arr[0].length;
                 if (cols == 0)
                         return false;
-
                 int i = 0, j = cols - 1;
 
                 while (i < rows && j >= 0) {
                         if (arr[i][j] == target) {
                                 return true;
                         }
-
                         else if (arr[i][j] > target) {
                                 j--;
                         }
-
                         else {
                                 i++;
                         }
                 }
-
                 return false;
         }
 }
