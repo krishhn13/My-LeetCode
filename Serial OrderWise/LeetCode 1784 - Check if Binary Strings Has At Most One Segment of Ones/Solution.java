fc19c12da380c35dc9bbf35181a2d69ea1ed9823
Krishan <chiranjiv.shah783@gmail.com>
2026-03-06 17:52:08 +0530

    Released Sol for LC 1784

--- Serial OrderWise/LeetCode 1784 - Check if Binary Strings Has At Most One Segment of Ones/Solution.java
+++ Serial OrderWise/LeetCode 1784 - Check if Binary Strings Has At Most One Segment of Ones/Solution.java
@@ -1,15 +1,16 @@
 class Solution {
-    public boolean checkOnesSegment(String s) {
-        boolean found = false;
-        for(int i = 0; i < s.length(); i++) {
-            if(s.charAt(i) == '1' && !found) {
-                while(i < s.length() && s.charAt(i) == '1') {
-                    i++;
+        public boolean checkOnesSegment(String s) {
+                boolean found = false;
+                for (int i = 0; i < s.length(); i++) {
+                        if (s.charAt(i) == '1' && !found) {
+                                while (i < s.length() && s.charAt(i) == '1') {
+                                        i++;
+                                }
+                                i--;
+                                found = true;
+                        } else if (s.charAt(i) == '1' && found)
+                                return false;
                 }
-                i--;
-                found = true;
-            } else if(s.charAt(i) == '1' && found) return false;
+                return true;
         }
-        return true;
-    }
 }
