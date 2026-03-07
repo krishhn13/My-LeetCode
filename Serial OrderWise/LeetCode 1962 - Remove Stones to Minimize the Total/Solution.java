e640cd3ebf0e602ef65396841760382c9620385e
Krishan <chiranjiv.shah783@gmail.com>
2026-03-07 18:53:15 +0530

    Released Solution for LeetCode 1962

---
+++ Serial OrderWise/LeetCode 1962 - Remove Stones to Minimize the Total/Solution.java
@@ -0,0 +1,22 @@
+class Solution {
+        public int minStoneSum(int[] piles, int k) {
+                PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
+                for (int i : piles) {
+                        pq.add(i);
+                }
+                while (k > 0) {
+                        int val = pq.poll();
+                        if (val % 2 == 0) {
+                                pq.add(val / 2);
+                        } else {
+                                pq.add((val / 2) + 1);
+                        }
+                        k--;
+                }
+                int sum = 0;
+                while (!pq.isEmpty()) {
+                        sum += pq.poll();
+                }
+                return sum;
+        }
+}
