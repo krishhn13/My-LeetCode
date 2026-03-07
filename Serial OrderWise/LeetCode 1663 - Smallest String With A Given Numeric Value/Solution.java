17ce6d6d411ce74919b10031618bbdf6f893aa9c
Krishan <chiranjiv.shah783@gmail.com>
2026-03-07 18:51:22 +0530

    Released Solution for LeetCode 1668

---
+++ Serial OrderWise/LeetCode 1663 - Smallest String With A Given Numeric Value/Solution.java
@@ -0,0 +1,15 @@
+class Solution {
+    public String getSmallestString(int n, int k) {
+        StringBuilder sb = new StringBuilder();
+        while(n>0) {
+            int start = 26;
+            while(k - start < n-1) {
+                start--;
+            }
+            sb.append((char)('a'+start-1));
+            k-=start;
+            n-=1;
+        }
+        return sb.reverse().toString();
+    }
+}
