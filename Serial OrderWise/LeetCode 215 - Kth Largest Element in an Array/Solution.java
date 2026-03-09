3a4e392d200d50ff20144d660f3b9ebe7c6ffa56
Krishan <chiranjiv.shah783@gmail.com>
2026-03-09 21:41:21 +0530

    Released solution for LC 215

--- Serial OrderWise/LeetCode 215 - Kth Largest Element in an Array/Solution.java
+++ Serial OrderWise/LeetCode 215 - Kth Largest Element in an Array/Solution.java
@@ -1,12 +1,15 @@
+import java.util.*;
+
 class Solution {
-    public int findKthLargest(int[] nums, int k) {
-        PriorityQueue<Integer> pq = new PriorityQueue(Collections.reverseOrder());
-        for(int i : nums) pq.add(i);
-        int i = 1;
-        while(i<k){
-            pq.remove();
-            i++;
+        public int findKthLargest(int[] nums, int k) {
+                PriorityQueue<Integer> pq = new PriorityQueue(Collections.reverseOrder());
+                for (int i : nums)
+                        pq.add(i);
+                int i = 1;
+                while (i < k) {
+                        pq.remove();
+                        i++;
+                }
+                return pq.peek();
         }
-        return pq.peek();
-    }
 }
