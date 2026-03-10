9e11e0e2abeba817c06d0e8ea36ebe15428e278d
Krishan <chiranjiv.shah783@gmail.com>
2026-03-10 20:40:28 +0530

    Released Sol for LC 1711

--- Serial OrderWise/LeetCode 1711 - Count Good Meals/Solution.java
+++ Serial OrderWise/LeetCode 1711 - Count Good Meals/Solution.java
@@ -1,17 +1,19 @@
+import java.util.*;
+
 class Solution {
-    public int countPairs(int[] deliciousness) {
-        long cnt = 0;
-        int mod = (int)1e9+7;
-        Map<Integer,Integer> map = new HashMap<>();
-        for(int i = 0 ; i < deliciousness.length; i++) {
-            for(int j = 0; j < 22; j++) { 
-                int need = (1<<j)- deliciousness[i];
-                if(map.containsKey(need)) {
-                    cnt += map.get(need);
+        public int countPairs(int[] deliciousness) {
+                long cnt = 0;
+                int mod = (int) 1e9 + 7;
+                Map<Integer, Integer> map = new HashMap<>();
+                for (int i = 0; i < deliciousness.length; i++) {
+                        for (int j = 0; j < 22; j++) {
+                                int need = (1 << j) - deliciousness[i];
+                                if (map.containsKey(need)) {
+                                        cnt += map.get(need);
+                                }
+                        }
+                        map.put(deliciousness[i], map.getOrDefault(deliciousness[i], 0) + 1);
                 }
-            }
-            map.put(deliciousness[i], map.getOrDefault(deliciousness[i], 0) + 1);
+                return (int) (cnt % mod);
         }
-        return (int)(cnt%mod);
-    }
 }
