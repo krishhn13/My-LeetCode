e9b9f201360bd1139aad75bae8599e5be4b50b1a
Krishan <chiranjiv.shah783@gmail.com>
2026-03-11 20:10:35 +0530

    Released Solution for LC 1009

--- Serial OrderWise/LeetCode 1009 - Complement of Base 10 Integer/Solution.java
+++ Serial OrderWise/LeetCode 1009 - Complement of Base 10 Integer/Solution.java
@@ -1,12 +1,13 @@
 class Solution {
-    public int bitwiseComplement(int n) {
-        if(n == 0) return 1;
-        int pow = 0;
-        int ans = 1;
-        while(ans <= n) {
-            ans = 1<<pow;
-            pow++;
+        public int bitwiseComplement(int n) {
+                if (n == 0)
+                        return 1;
+                int pow = 0;
+                int ans = 1;
+                while (ans <= n) {
+                        ans = 1 << pow;
+                        pow++;
+                }
+                return ans - 1 - n;
         }
-        return ans - 1 - n;
-    }
 }
