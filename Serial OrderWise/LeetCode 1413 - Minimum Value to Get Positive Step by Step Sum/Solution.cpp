c26832b77c65e6332352846eacda4c351a3cd089
Krishan <chiranjiv.shah783@gmail.com>
2026-03-12 20:45:11 +0530

    Released CPP Solution for LC 1413

---
+++ Serial OrderWise/LeetCode 1413 - Minimum Value to Get Positive Step by Step Sum/Solution.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int minStartValue(vector<int>& nums) {
+        int sum = 0, mini = 0;
+        for(int num : nums) {
+            sum += num;
+            mini = mini > sum ? sum : mini;
+        }
+        return 1 - mini;
+    }
+};
