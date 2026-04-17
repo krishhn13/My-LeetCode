97d237f321ea4320c3a8f4a7d39eb63a9eb96c0c
Krishan <chiranjiv.shah783@gmail.com>
2026-04-17 20:50:46 +0530

    Released Solution for LeetCode 365

--- Serial OrderWise/LeetCode 365 - Water Jug Problem/Solution.java
+++ Serial OrderWise/LeetCode 365 - Water Jug Problem/Solution.java
@@ -1,15 +1,16 @@
 class Solution {
-    public boolean canMeasureWater(int x, int y, int target) {
-        if(target > x + y) {
-            return false;
+        public boolean canMeasureWater(int x, int y, int target) {
+                if (target > x + y) {
+                        return false;
+                }
+                int ele = gcd(x, y);
+                return target % ele == 0;
         }
-        int ele = gcd(x, y);
-        return target%ele == 0;
-    }
-    private int gcd(int x, int y) {
-        if(y == 0) {
-            return x;
+
+        private int gcd(int x, int y) {
+                if (y == 0) {
+                        return x;
+                }
+                return gcd(y, x % y);
         }
-        return gcd(y, x%y);
-    }
 }
