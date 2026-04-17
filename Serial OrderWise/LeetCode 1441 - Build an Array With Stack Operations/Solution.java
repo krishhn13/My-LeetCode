051623ee70f1bedeae8325eb522e2a10bf63e0df
Krishan <chiranjiv.shah783@gmail.com>
2026-04-17 20:53:00 +0530

    Released Solution for LeetCode 1441

--- Serial OrderWise/LeetCode 1441 - Build an Array With Stack Operations/Solution.java
+++ Serial OrderWise/LeetCode 1441 - Build an Array With Stack Operations/Solution.java
@@ -1,20 +1,21 @@
 import java.util.*;
+
 class Solution {
-    public List<String> buildArray(int[] target, int n) {
-        List<String> list = new ArrayList<>();
-        int el = 0;
-        for(int i = 1; i <= n; i++) {
-            if(el == target.length) {
-                break;
-            }
-            if(i == target[el]) {
-                list.add("Push");
-                el++;
-            } else {
-                list.add("Push");
-                list.add("Pop");
-            }
+        public List<String> buildArray(int[] target, int n) {
+                List<String> list = new ArrayList<>();
+                int el = 0;
+                for (int i = 1; i <= n; i++) {
+                        if (el == target.length) {
+                                break;
+                        }
+                        if (i == target[el]) {
+                                list.add("Push");
+                                el++;
+                        } else {
+                                list.add("Push");
+                                list.add("Pop");
+                        }
+                }
+                return list;
         }
-        return list;
-    }
 }
