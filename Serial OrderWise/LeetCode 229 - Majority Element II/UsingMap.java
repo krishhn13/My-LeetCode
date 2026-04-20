266626e51f86e09e41114a26c18b986c03e99fda
Krishan <chiranjiv.shah783@gmail.com>
2026-04-20 15:14:00 +0530

    Released SOl for LC 229

---
+++ Serial OrderWise/LeetCode 229 - Majority Element II/UsingMap.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+public class UsingMap {
+        static class Solution {
+                public List<Integer> majorityElement(int[] nums) {
+                        List<Integer> list = new ArrayList<>();
+                        Map<Integer,Integer> map = new HashMap<>();
+                        int n = nums.length;
+                        for(int i = 0; i < nums.length; i++) {
+                                map.put(nums[i], map.getOrDefault(nums[i], 0)+1);
+                                if(map.get(nums[i])>n/3) {
+                                        list.add(nums[i]);
+                                }
+                        }
+                        return list;
+                }
+        }
+        public static void main(String[] args) {
+                int[] nums = {3,2,3};
+                Solution sol = new Solution();
+                System.out.println(sol.majorityElement(nums));
+        }
+}
