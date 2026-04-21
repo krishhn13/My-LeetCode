ad070c2a584de5ee24f6c105703fa69357617fe5
Krishan <chiranjiv.shah783@gmail.com>
2026-04-21 21:18:08 +0530

    Released Solution for LC 567

--- Serial OrderWise/LeetCode 567 - Permutation in String/Solution.java
+++ Serial OrderWise/LeetCode 567 - Permutation in String/Solution.java
@@ -1,20 +1,25 @@
 import java.util.*;
+
 class Solution {
         public boolean checkInclusion(String s1, String s2) {
-            if (s1.length() > s2.length()) return false;
-            int[] freq = new int[26];
-            int[] freq2 = new int[26];
-            for (char c : s1.toCharArray()) freq[c - 'a']++;
-            for (int i = 0; i < s1.length(); i++) {
-                freq2[s2.charAt(i) - 'a']++;
-            }
-            if (Arrays.equals(freq, freq2)) return true;
-            int j = s1.length();
-            for (int i = j; i < s2.length(); i++) {
-                freq2[s2.charAt(i - j) - 'a']--;
-                freq2[s2.charAt(i) - 'a']++;
-                if (Arrays.equals(freq, freq2)) return true;
-            }
-            return false;
+                if (s1.length() > s2.length())
+                        return false;
+                int[] freq = new int[26];
+                int[] freq2 = new int[26];
+                for (char c : s1.toCharArray())
+                        freq[c - 'a']++;
+                for (int i = 0; i < s1.length(); i++) {
+                        freq2[s2.charAt(i) - 'a']++;
+                }
+                if (Arrays.equals(freq, freq2))
+                        return true;
+                int j = s1.length();
+                for (int i = j; i < s2.length(); i++) {
+                        freq2[s2.charAt(i - j) - 'a']--;
+                        freq2[s2.charAt(i) - 'a']++;
+                        if (Arrays.equals(freq, freq2))
+                                return true;
+                }
+                return false;
         }
-    }
+}
