3449348f4bee672a8e99e2eeb12ce7f9221fec3a
Krishan <chiranjiv.shah783@gmail.com>
2026-04-23 19:14:51 +0530

    Updated Solution for LeetCode 2615 - Structured the solution, added indentations to make the code look pretty

--- Serial OrderWise/LeetCode 1855 - Maximum Distance Between a Pair of Values/Solution.java
+++ Serial OrderWise/LeetCode 1855 - Maximum Distance Between a Pair of Values/Solution.java
@@ -1,16 +1,17 @@
 class Solution {
-    public int maxDistance(int[] nums1, int[] nums2) {
-        int maxi = 0;
-        int i = 0, j = 0;
-        while(i < nums1.length && j < nums2.length) {
-            if(nums1[i] <= nums2[j]) {
-                maxi = Math.max(maxi, j - i);
-                j++;
-            } else {
-                i++;
-                if(i > j) j = i;
-            }
+        public int maxDistance(int[] nums1, int[] nums2) {
+                int maxi = 0;
+                int i = 0, j = 0;
+                while (i < nums1.length && j < nums2.length) {
+                        if (nums1[i] <= nums2[j]) {
+                                maxi = Math.max(maxi, j - i);
+                                j++;
+                        } else {
+                                i++;
+                                if (i > j)
+                                        j = i;
+                        }
+                }
+                return maxi;
         }
-        return maxi; 
-    }
 }
