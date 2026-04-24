776d5f6128f4a5d97edc6f3d8bf0f5bca0cea3bd
Krishan <chiranjiv.shah783@gmail.com>
2026-04-24 22:01:34 +0530

    Released Solution for LC 235

---
+++ Serial OrderWise/LeetCode 235 - Lowest Common Ancestor of a Binary Search Tree/Solution.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+class Solution {
+        public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+                if(root == null) {
+                        return null;
+                }
+                if(p.val > root.val && q.val > root.val) {
+                        return lowestCommonAncestor(root.right, p, q);
+                } else if(p.val < root.val && q.val < root.val) {
+                        return lowestCommonAncestor(root.left, p, q);
+                }
+                return root;
+        }
+}
