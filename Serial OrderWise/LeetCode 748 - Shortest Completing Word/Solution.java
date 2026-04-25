0018188bace0791f72da24aa6e6204ce44e8f416
Krishan <chiranjiv.shah783@gmail.com>
2026-04-25 21:10:25 +0530

    Released Solution for LC 748

--- Serial OrderWise/LeetCode 748 - Shortest Completing Word/Solution.java
+++ Serial OrderWise/LeetCode 748 - Shortest Completing Word/Solution.java
@@ -1,29 +1,29 @@
 class Solution {
-    public String shortestCompletingWord(String licensePlate, String[] words) {
-        int[] alpha = new int[26]; 
-        for(char c : licensePlate.toCharArray()) {
-            if(Character.isLetter(c)) {
-                char lowerChar = Character.toLowerCase(c);
-                alpha[lowerChar - 'a']++;
-            }
-        }
-        String ans = "";
-        for(String word : words) {
-            int[] wordFreq = new int[26];
-            for(char c : word.toCharArray()) {
-                wordFreq[c - 'a']++;
-            }
-            boolean isValid = true;
-            for(int i = 0; i < 26; i++) {
-                if(alpha[i] > 0 && wordFreq[i] < alpha[i]) {
-                    isValid = false;
-                    break;
+        public String shortestCompletingWord(String licensePlate, String[] words) {
+                int[] alpha = new int[26];
+                for (char c : licensePlate.toCharArray()) {
+                        if (Character.isLetter(c)) {
+                                char lowerChar = Character.toLowerCase(c);
+                                alpha[lowerChar - 'a']++;
+                        }
                 }
-            }
-            if(isValid && (ans.isEmpty() || word.length() < ans.length())) {
-                ans = word;
-            }
+                String ans = "";
+                for (String word : words) {
+                        int[] wordFreq = new int[26];
+                        for (char c : word.toCharArray()) {
+                                wordFreq[c - 'a']++;
+                        }
+                        boolean isValid = true;
+                        for (int i = 0; i < 26; i++) {
+                                if (alpha[i] > 0 && wordFreq[i] < alpha[i]) {
+                                        isValid = false;
+                                        break;
+                                }
+                        }
+                        if (isValid && (ans.isEmpty() || word.length() < ans.length())) {
+                                ans = word;
+                        }
+                }
+                return ans;
         }
-        return ans;
-    }
 }
