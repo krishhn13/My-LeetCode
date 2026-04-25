d2315c0fbb4a8f3cb275c04064ee0476696de30e
Krishan <chiranjiv.shah783@gmail.com>
2026-04-25 21:12:18 +0530

    Released Sol for LC 2560

--- Serial OrderWise/LeetCode 2560 - House Robber IV/Solution.java
+++ Serial OrderWise/LeetCode 2560 - House Robber IV/Solution.java
@@ -1,29 +1,31 @@
+import java.util.*;
+
 class Solution {
-    private boolean canStealKHouses(int[] nums, int k, int capability) {
-        int count = 0;
-        int i = 0;
-        while (i < nums.length) {
-            if (nums[i] <= capability) {
-                count++;
-                i += 2;
-            } else {
-                i++;
-            }
+        private boolean canStealKHouses(int[] nums, int k, int capability) {
+                int count = 0;
+                int i = 0;
+                while (i < nums.length) {
+                        if (nums[i] <= capability) {
+                                count++;
+                                i += 2;
+                        } else {
+                                i++;
+                        }
+                }
+                return count >= k;
         }
-        return count >= k;
-    }
-    
-    public int minCapability(int[] nums, int k) {
-        int left = Arrays.stream(nums).min().getAsInt();
-        int right = Arrays.stream(nums).max().getAsInt();
-        while (left < right) {
-            int mid = left + (right - left) / 2;
-            if (canStealKHouses(nums, k, mid)) {
-                right = mid;
-            } else {
-                left = mid + 1;
-            }
+
+        public int minCapability(int[] nums, int k) {
+                int left = Arrays.stream(nums).min().getAsInt();
+                int right = Arrays.stream(nums).max().getAsInt();
+                while (left < right) {
+                        int mid = left + (right - left) / 2;
+                        if (canStealKHouses(nums, k, mid)) {
+                                right = mid;
+                        } else {
+                                left = mid + 1;
+                        }
+                }
+                return left;
         }
-        return left;
-    }
 }
