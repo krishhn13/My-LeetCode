79c5f21eba5ca54d581ae4bba4de3b0f774a6638
Krishan <chiranjiv.shah783@gmail.com>
2026-04-28 21:00:28 +0530

    Released Solution for LC 456

--- Serial OrderWise/LeetCode 456 - 132 Pattern/Solution.java
+++ Serial OrderWise/LeetCode 456 - 132 Pattern/Solution.java
@@ -1,21 +1,23 @@
 class Solution {
-    public boolean find132pattern(int[] nums) {
-        int n = nums.length;
-        if (n < 3) return false;
+        public boolean find132pattern(int[] nums) {
+                int n = nums.length;
+                if (n < 3)
+                        return false;
 
-        Stack<Integer> stack = new Stack<>();
-        int third = Integer.MIN_VALUE;
+                Stack<Integer> stack = new Stack<>();
+                int third = Integer.MIN_VALUE;
 
-        for (int i = n - 1; i >= 0; i--) {
-            if (nums[i] < third) return true;  
+                for (int i = n - 1; i >= 0; i--) {
+                        if (nums[i] < third)
+                                return true;
 
-            while (!stack.isEmpty() && nums[i] > stack.peek()) {
-                third = stack.pop();
-            }
+                        while (!stack.isEmpty() && nums[i] > stack.peek()) {
+                                third = stack.pop();
+                        }
 
-            stack.push(nums[i]); 
+                        stack.push(nums[i]);
+                }
+
+                return false;
         }
-
-        return false;
-    }
 }
