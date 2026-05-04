6a7b732f90e925b9b447418bc75e4161b618550b
Krishan <chiranjiv.shah783@gmail.com>
2026-05-04 15:45:12 +0530

    Released Solution for Lc 382

---
+++ Serial OrderWise/LeetCode 382 - Linked List Random Node/Solution.java
@@ -0,0 +1,21 @@
+class Solution {
+    List<Integer> temp ;
+    public Solution(ListNode head) {
+        temp = new ArrayList<>();
+        ListNode tmp = head;
+        while(tmp!=null) {
+            temp.add(tmp.val);
+            tmp = tmp.next;
+        }
+    }
+    public int getRandom() {
+        int randomIndex = new Random().nextInt(temp.size());
+        return temp.get(randomIndex);
+    }
+}
+
+/**
+ * Your Solution object will be instantiated and called as such:
+ * Solution obj = new Solution(head);
+ * int param_1 = obj.getRandom();
+ */
