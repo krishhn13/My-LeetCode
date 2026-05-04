afd02d1e8f4b42bb8ace0dcc7ab363e5a052fcb1
Krishan <chiranjiv.shah783@gmail.com>
2026-05-04 15:46:50 +0530

    Released Solution for LC 2487

--- Serial OrderWise/LeetCode 2487 - Remove Nodes From Linked List/Solution.java
+++ Serial OrderWise/LeetCode 2487 - Remove Nodes From Linked List/Solution.java
@@ -1,26 +1,27 @@
 class Solution {
-    public ListNode removeNodes(ListNode head) {
-        ListNode revHead = reverse(head); 
-        int maxi = revHead.val;
-        ListNode curr = revHead;
-        while (curr != null && curr.next != null) {
-            if (curr.next.val < maxi) {
-                curr.next = curr.next.next; 
-            } else {
-                curr = curr.next;
-                maxi = curr.val;
-            }
+        public ListNode removeNodes(ListNode head) {
+                ListNode revHead = reverse(head);
+                int maxi = revHead.val;
+                ListNode curr = revHead;
+                while (curr != null && curr.next != null) {
+                        if (curr.next.val < maxi) {
+                                curr.next = curr.next.next;
+                        } else {
+                                curr = curr.next;
+                                maxi = curr.val;
+                        }
+                }
+                return reverse(revHead);
         }
-        return reverse(revHead);
-    }
-    private ListNode reverse(ListNode head) {
-        ListNode temp = head, prev = null;
-        while(temp != null) {
-            ListNode fwd = temp.next;
-            temp.next = prev;
-            prev = temp;
-            temp = fwd;
+
+        private ListNode reverse(ListNode head) {
+                ListNode temp = head, prev = null;
+                while (temp != null) {
+                        ListNode fwd = temp.next;
+                        temp.next = prev;
+                        prev = temp;
+                        temp = fwd;
+                }
+                return prev;
         }
-        return prev;
-    }
 }
