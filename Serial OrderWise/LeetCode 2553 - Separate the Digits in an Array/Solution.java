01bd94e01d9457bc3598da22a989ba91481cfc6c
Krishan <chiranjiv.shah783@gmail.com>
2026-05-12 17:39:42 +0530

    Released LC 2553

--- Serial OrderWise/LeetCode 2553 - Separate the Digits in an Array/Solution.java
+++ Serial OrderWise/LeetCode 2553 - Separate the Digits in an Array/Solution.java
@@ -1,13 +1,13 @@
 class Solution {
-    public int[] separateDigits(int[] nums) {
-        StringBuilder str = new StringBuilder();
-        for(int i : nums) {
-            str.append(i);
+        public int[] separateDigits(int[] nums) {
+                StringBuilder str = new StringBuilder();
+                for (int i : nums) {
+                        str.append(i);
+                }
+                int[] ans = new int[str.length()];
+                for (int i = 0; i < ans.length; i++) {
+                        ans[i] = str.charAt(i) - '0';
+                }
+                return ans;
         }
-        int[] ans = new int[str.length()];
-        for(int i = 0; i < ans.length; i++) {
-            ans[i] = str.charAt(i) - '0';
-        }
-        return ans;
-    }
-}
+}       
